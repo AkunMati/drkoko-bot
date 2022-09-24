d356195beba301edb6d7e87968cf512db58f246c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-09-25 00:07:28 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
-    pepe = 'https://i.ibb.co/BzR1jRq/08a088fe953b.jpg'
+    pepe = 'https://i.ibb.co/1LF6QHQ/pangeran.jpg'
     baper = await fetch(pepe).then(a => a.buffer())
     let listMessage = {
-        "title": "◈ Made by The.sad.boy01",
+        "title": "◈ Made by ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×",
         "description": `
 ┌「 *Sewa bot* 」
 │     
@@ -17,7 +17,7 @@ let handler = async (m, { conn }) => {
 ├ Rp. 10,000 Pulsa Three
 │
 ├ tertarik? hubungi: 
-├ @62895336282144 (The.sad.boy01)
+├ @6287740530274 (ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×)
 └────
 `.trim(),
         "listType": "PRODUCT_LIST",
@@ -36,9 +36,9 @@ let handler = async (m, { conn }) => {
                 "productId": "4730029423700586",
                 "jpegThumbnail": baper
             },
-            "businessOwnerJid": "6288217277973@s.whatsapp.net"
+            "businessOwnerJid": "6287740530274@s.whatsapp.net"
         },
-        "footerText": "https://wa.me/c/6288217277973\n\nowner number : wa.me/62895336282144"
+        "footerText": "https://wa.me/c/6287740530274\n\nowner number : wa.me/6287740530274"
     }
     conn.sendMessage(m.chat, listMessage, 'listMessage', { quoted: m, contextInfo: { mentionedJid: conn.parseMention(listMessage.description) } })
 }
