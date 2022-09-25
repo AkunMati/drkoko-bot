7bf6f3f81a17c45b6a58b90bc7ffe468ede9c982
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-09-25 19:24:19 +0700

    Update g-tagall.js

--- plugins/g-tagall.js
+++ plugins/g-tagall.js
@@ -3,16 +3,17 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
                 global.dfail('admin', m, conn)
                 throw false
                 }
-  let teks = `${text ? text : ''}\n\n┌─「 Tag All 」\n`
+  let teks = `┌─「 TAG ALL 」\n\n@${m.sender.split('@')[0]} : ${text ? text : ''}\n\n`
   for (let mem of participants) {
-  teks += `├ @${mem.id.split('@')[0]}\n`}
+  teks += `├✪➢ @${mem.id.split('@')[0]}\n`}
   teks += `└────\n`
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <message>']
 handler.tags = ['group']
 handler.command = /^(t(agall)?)$/i
-
+handler.botAdmin = true
+handler.admin = true
 handler.group = true
 
 module.exports = handler
