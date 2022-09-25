bf834613215a2523d204c7fc691bd7340a8a6cfc
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-09-25 18:59:50 +0700

    Update o-unbanchat.js

--- plugins/o-unbanchat.js
+++ plugins/o-unbanchat.js
@@ -18,6 +18,37 @@ let handler = async (m, { conn, isOwner, text, isAdmin }) => {
   try {
     if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = false
     else global.db.data.users[who].banned = false
+    m.reply(`Berhasil unban! ${await conn.user.name} aktif dichat ${await conn.getName(who) == undefined ? 'ini' : await conn.getName(who)}.`)
+  } catch (e) {
+    throw `nomor tidak ada didatabase!`
+  }
+}
+handler.help = ['unbanchat']
+handler.tags = ['owner', 'group']
+handler.command = /^(unbanchat)$/i
+
+module.exports = handler
+ 	
+/*let handler = async (m, { conn, isOwner, text, isAdmin }) => {
+  let who
+  if (m.isGroup) {
+    if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn)
+      throw false
+    }
+    if (isOwner) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    else who = m.chat
+  } else {
+    if (!isOwner) {
+      global.dfail('owner', m, conn)
+      throw false
+    }
+    who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+  }
+
+  try {
+    if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = false
+    else global.db.data.users[who].banned = false
     conn.sendButtonDoc(m.chat, `Berhasil unban! ${await conn.user.name} aktif dichat ${await conn.getName(who) == undefined ? 'ini' : await conn.getName(who)}.`, wm, 'Owner', '.owner', m)
   } catch (e) {
     throw `nomor tidak ada didatabase!`
@@ -28,4 +59,4 @@ handler.tags = ['owner', 'group']
 handler.command = /^unban(chat)?|unbco$/i
 handler.owner = true
 handler.admin = false
-module.exports = handler
+module.exports = handler*/
