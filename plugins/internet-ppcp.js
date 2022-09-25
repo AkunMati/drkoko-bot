d7ded963f170b55e686d471b1c10b29b6f4214f1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-09-25 14:12:44 +0700

    Update internet-ppcp.js

--- plugins/internet-ppcp.js
+++ plugins/internet-ppcp.js
@@ -1,4 +1,4 @@
-let fetch = require("node-fetch")
+/*let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
 let res = await fetch('https://api.xteam.xyz/randomimage/ppcouple?APIKEY=NezukoTachibana281207')
@@ -11,6 +11,16 @@ conn.sendFile(m.chat, json.result.male, 'ppcowo.png', 'Cowok', m, false,{ contex
 
 conn.sendFile(m.chat, json.result.female, 'ppcewe.png', 'Cewek', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
+}*/
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command }) => {
+  let res = await fetch('https://api.xteam.xyz/randomimage/ppcouple?APIKEY=NezukoTachibana281207')
+  if (res.status != 200) throw await res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+conn.sendButtonImg(m.chat, json.result.female,  'PP Cewenya', wm, 'Lanjut?', '.ppcp', m)
+conn.sendFile(m.chat, json.result.male,'pria.jpg',  'PP Cowonya', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet', 'anime']
