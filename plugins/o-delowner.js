f12a33c0ac5bf358d71a3a80c00c2f952056f461
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-09-28 13:02:05 +0700

    Update o-delowner.js

--- plugins/o-delowner.js
+++ plugins/o-delowner.js
@@ -1,4 +1,28 @@
-let fs = require('fs')
+let { MessageType } = require('@adiwajshing/baileys') 
+let handler = async (m, { conn, text }) => {
+    if (!text) throw 'Siapa kak yg mau di demote?'
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let user = global.db.data.users[who]
+    user.owner = false
+    
+    conn.reply(m.chat, `Hai, @${who.split`@`[0]}. kamu sudah di demote`, m, { mentions: [who] }, {
+        contextInfo: {
+            mentionedJid: [who]
+        }
+    })
+  
+}
+handler.help = ['downer *@user*']
+handler.tags = ['owner']
+handler.command = /^downer$/i
+handler.rowner = true
+module.exports = handler
+
+
+
+
+
+/*let fs = require('fs')
 let handler = async (m, { conn, args }) => {
 
     const json = JSON.parse(fs.readFileSync('./src/owner.json'))
@@ -21,4 +45,4 @@ handler.command = /^(remove|hapus|-|del)owner$/i
 
 handler.rowner = true
 
-module.exports = handler
+module.exports = handler*/
