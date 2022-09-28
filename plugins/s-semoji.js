40be4ab967f3dab35b3b2044191c1bcf2710e1d4
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-09-28 13:03:29 +0700

    Update s-semoji.js

--- plugins/s-semoji.js
+++ plugins/s-semoji.js
@@ -1,4 +1,69 @@
-const { EmojiAPI } = require("emoji-api")
+// Terimakasih kpd RC047 :v
+// Fitur By Xteams
+// Modified by Nurutomo (Update Xteam :v)
+
+const { sticker } = require('../lib/sticker')
+const fetch = require('node-fetch')
+const defaultType = 'whatsapp'
+let handler = async (m, { usedPrefix, conn, args, text }) => {
+  let [tipe, emoji] = text.includes('|') ? text.split('|') : args
+  if (tipe && !emoji) {
+    emoji = tipe
+    tipe = defaultType
+  }
+  if (!emoji) throw `
+Silahkan masukan emojinya
+
+Misal ${usedPrefix}semoji whatsapp 😎
+
+List Tipe:
+${[
+  "apple",
+  "google",
+  "samsung",
+  "microsoft",
+  "whatsapp",
+  "twitter",
+  "facebook",
+  "joypixels",
+  "openmoji",
+  "emojidex",
+  "messenger",
+  "lg",
+  "htc",
+  "mozilla",
+  "softbank",
+  "docomo",
+  "au_by_kddi"
+].map(v => `- ${v}`).join('\n')}
+`.trim()
+  emoji = emoji.trim()
+  tipe = tipe.trim().toLowerCase()
+
+  let res = await fetch(global.API('xteam', '/sticker/emojitopng', { emo: emoji.trim() }, 'APIKEY'))
+  if (!res.ok) throw res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  if (!(tipe in json.result)) tipe = defaultType
+  let stiker = await sticker(null, json.result[tipe].img, global.packname, global.author, [emoji], { name: json.result[tipe].name })
+  //   m.reply(`
+  // Tipe: ${tipe}
+  // Emoji: ${emoji}
+  // `.trim())
+  m.reply(stiker)
+}
+handler.help = ['semoji [tipe] <emoji>']
+handler.tags = ['sticker']
+handler.command = /^s?emo(ji)?$/i
+module.exports = handler
+
+
+
+
+
+
+
+/*const { EmojiAPI } = require("emoji-api")
 const emoji = new EmojiAPI()
 let handler = async(m, { conn, usedPrefix, command, args, text, }) => {
     if(!text) throw `Example : *${usedPrefix + command} 😳*`
@@ -160,4 +225,4 @@ handler.help = ['semoji'].map(v => v + ' <emoji>')
 handler.tags = ['sticker']
 handler.command = /^((s(tic?ker)?)?emo(ji)?)$/i
 
-module.exports = handler
+module.exports = handler*/
