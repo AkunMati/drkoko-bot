70a43d88894793bcd19a79d51cf7474c54688ed2
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-09-28 12:48:33 +0700

    Update o-addowner.js

--- plugins/o-addowner.js
+++ plugins/o-addowner.js
@@ -1,4 +1,31 @@
-let fs = require('fs')
+let { MessageType } = require('@adiwajshing/baileys') 
+let handler = async (m, { conn, text }) => {
+    if (!text) throw 'Siapa kak yg mau dijadikan Owner?'
+    let who
+    if (m.isGroup) who = m.mentionedJid[0]
+    else who = m.chat
+    let user = global.db.data.users[m.sender]
+    user.owner = true
+    if (global.owner.includes(who.split`@`[0])) throw 'Sudah Owner!'
+    global.owner.push(`${who.split`@`[0]}`)
+    
+    
+    conn.reply(m.chat, `Hai, @${who.split`@`[0]}. Kamu sudah menjadi Owner, jangan iseng ya atau di demote!`, m, {
+        contextInfo: {
+            mentionedJid: [who]
+        }
+    })
+  
+}
+handler.help = ['addowner *@user*']
+handler.tags = ['owner']
+handler.command = /^(add|tambah|\+)owner$/i
+handler.rowner = true
+module.exports = handler
+
+
+
+/*let fs = require('fs')
 let handler = async (m, { conn, args }) => {
 
     const json = JSON.parse(fs.readFileSync('./src/owner.json'))
@@ -20,4 +47,4 @@ handler.command = /^(add|tambah|\+)ow(ner)?$/i
 
 handler.owner = true
 
-module.exports = handler
+module.exports = handler*/
