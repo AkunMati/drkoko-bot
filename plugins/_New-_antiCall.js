5f0105c29fa93ab48827836ec194594f209817d7
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-05 15:35:11 +0700

    Update _New-_antiCall.js

--- plugins/_New-_antiCall.js
+++ plugins/_New-_antiCall.js
@@ -3,7 +3,7 @@ let handler = m => m
 const { WAMessageStubType } = require('@adiwajshing/baileys')
 const { format } = require('util')
 
-let imgr = flaaa.getRandom()
+let imgr = pickRandom(flaaa)
 
 const isNumber = x => typeof x === 'number' && !isNaN(x)
 const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
@@ -18,7 +18,7 @@ handler.all = async function (m) {
 	if(!setting.anticall) return 
 	
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await conn.sendButton(m.chat, `👋 Kamu di Blockir karena menelepon *Bot*\n`,wm + '\n\n' + botdate, `${imgr + 'Anti Call'}`, [['OK','ok']], null)
+		await conn.sendButton(m.chat, `👋 Kamu di Blockir karena menelepon *Bot*\n`,wm + '\n\n' + run, `${imgr + 'Anti Call'}`, [['OK','ok']], null)
 		await delay(1000)
 		await this.updateBlockStatus(m.chat, "block")
 	}
