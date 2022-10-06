a73cb46d99167f10d955bc31677435a0f59e0376
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-07 00:38:54 +0700

    Create sound120.js

---
+++ plugins/sound120.js
@@ -0,0 +1,22 @@
+//import fs from 'fs'
+//import fetch from 'node-fetch'
+let fs = require('fs')
+let fetch = require('node-fetch')
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp3/soundultah.mp3')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ",
+    mediaType: 2,
+    description: "https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ", 
+    title: '©ᵈʳкσкσ-ᴍᴅ | ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ',
+    body: run,
+    thumbnail: thumb,
+    sourceUrl: data.sc  }}})
+}
+handler.command = ['soundultah']
+handler.help = ['soundultah']
+handler.tags = ['sounds']
+module.exports = handler
