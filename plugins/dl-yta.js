edb8ca7025343873f10ccda0f5e812173138bbe1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-07 17:53:12 +0700

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,4 +1,3 @@
-let limit = 30
 let fetch = require('node-fetch')
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
