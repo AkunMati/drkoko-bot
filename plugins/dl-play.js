70a0d3b2db9644fb1480de29f379dc2ae41056d8
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-08 16:30:58 +0700

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,103 +1,82 @@
-/*const { servers, yta, ytv } = require('../lib/y2mate')
-let yts = require('yt-search')
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `Harap masukkan query!\n\nContoh: ${usedPrefix + command} yanagi nagi one's hope`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let yt2 = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
-    }
-  }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonImg(m.chat, await (await fetch(thumb)).buffer(), `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-`.trim(), wm, 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
+let { youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
+let { servers, yta } = require('../lib/y2mate.js')
+let axios = require('axios')
+
+let handler = async (m, { conn, args, usedPrefix, command, isPrems, expiration }) => {
+   if (!args[0]) throw `${set.sb} *Example* : ${usedPrefix + command} url`
+   let ras = `Url salah, perintah ini untuk mengunduh audio youtube (watch/shorts)`
+   if (!args[0].match(/(https|http):\/\/(www.youtube.com|www.youtube|www.youtu.be|youtube.com|youtu.be.com|youtu.be)\/(watch|shorts)?/gi)) throw ras
+   m.react('⏱️')
+   let limit = isPrems ? 300 : 100 
+   let doc = db.data.chats[m.chat].asDocument
+   let qua = '128kbps'
+   let isLimit
+   try {
+     let res = await youtubedl(args[0])
+     isLimit = limit * 1024 < res.audio[qua].fileSize
+   } catch {
+     try {
+       let res = await youtubedlv2(args[0])
+       isLimit = limit * 1024 < res.audio[qua].fileSize
+     } catch {
+       try {
+         let res = await youtubedlv3(args[0])
+         isLimit = limit * 1024 < res.audio[qua].fileSize
+       } catch {
+         let server = (args[1] || servers[0]).toLowerCase()
+         let anu = await yta(args[0], servers.includes(server) ? server : servers[0])
+         isLimit = limit * 1024 < anu.filesize 
+       }
+     }
+   }
+   let res = await youtubedl(args[0])
+   let media 
+   try { 
+     media = await res.audio[qua].download()
+   } catch {
+     console.log('yt1 eror mencoba yt2')
+     try {       
+        let res2 = await youtubedlv2(args[0])
+        media = await res2.audio[qua].download()
+     } catch {
+       console.log('yt2 eror mencoba yt3')
+       try {
+          let res3 = await youtubedlv2(args[0])
+          media = await res3.audio[qua].download()
+       } catch {
+         console.log('yt1l3 eror mencoba yt4')
+         let server = (args[1] || servers[0]).toLowerCase()
+         let anu = await yta(args[0], servers.includes(server) ? server : servers[0])
+         media = anu.dl_link
+       }
+     }
+   }
+   let anu
+   if (isLimit) anu = `_File size above average ${limit} MB download it yourself_\n${await(await axios.get(`https://tinyurl.com/api-create.php?url=${media}`)).data}`                 
+   else anu = `_Wait for the audio to be sent it may take a few minutes!_`
+   let capt = `${set.sa}  *Y T   M P 3*
+   
+${set.sb} *Title* : ${res.title ? res.title : 'Not found'}
+${set.sb} *Size* : ${res.audio[qua]?.fileSizeH ? res.audio[qua]?.fileSizeH : 'Not found'}
+${set.sb} *Quality* : ${res.audio[qua]?.quality ? res.audio[qua]?.quality : 'Not found'}
+${anu}
+  `
+   let sentMsg = await conn.reply(m.chat, capt, m, {
+     ephemeralExpiration: expiration,
+     contextInfo: {
+       externalAdReply :{
+         mediaType: 1,
+         title: wm, 
+         thumbnail: await conn.getBuffer(res.thumbnail),
+         renderLargerThumbnail: true,
+       }
+     }
+   })
+   if (!isLimit) conn.sendFile(m.chat, media, res.title + '.mp3', res.title, sentMsg, null, { asDocument: doc, mentions: [m.sender] })
 }
-handler.help = ['play'].map(v => v + ' <pencarian>')
+handler.help = ['ytmp3'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(play)$/i
-handler.register = true
-handler.exp = 0
-
-module.exports = handler*/
-
-let yts = require('yt-search')
-let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => {
-  if (!text) throw `What song are you want find?\n*Example:* ${usedPrefix}play alan walker faded`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Not Found'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry another server...'}`)
-    }
-  }
-  if (yt === false) throw 'All servers can\'t 😕'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
-*Server y2mate:* ${usedServer}
-_*Please wait while processing..*_
-`.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
-}
-handler.help = ['play', 'play2'].map(v => v + ' <search>')
-handler.tags = ['downloader']
-handler.command = /^play2?$/i
-handler.premium = false
-handler.group = true
-
-handler.exp = 0
-handler.limit = true
-
+handler.command = /(y(ou)?t(ube)?(a(udio)?|mpp?3|musik))$/i
+handler.limit = true 
+handler.desc = ['Mendownload media audio dari Youtube, gunakan perintah *#ytmp3 url* hilangkan tanda < >']
 module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
