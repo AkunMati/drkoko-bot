f34d71e6c6121ad20c09d397bd7d60f3f503bc7b
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-08 17:38:17 +0700

    Update _New-_antiCall.js

--- plugins/_New-_antiCall.js
+++ plugins/_New-_antiCall.js
@@ -11,25 +11,14 @@ const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function (
     resolve()
 }, ms))
 
-handler.all = async function (m, { conn }) {
-	conn.p = conn.p ? conn.p : {}
-	let id = m.chat
-	conn.p[id] = [
-	await conn.sendKontak(m.chat, data.kontak, m, { contextInfo: { externalAdReply :{
-    showAdAttribution: true,
-     }}
-  })
-	]
-	await delay(100)
-        return delete conn.p[id]
-}
+handler.all = async function (m) { 
 	if (m.fromMe && m.isBaileys) return !0
 	let text;
 	let setting = global.db.data.settings[this.user.jid]
 	if(!setting.anticall) return 
 	
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await conn.sendButton(m.chat, `👋 Kamu di Blockir karena menelepon *Bot*\n\nChat Owner *Bot* Suruh Buka Blokiran:\nhttps://wa.me/6287740530274`,wm + '\n\n' + run, 'KOKO', '.owner', null)
+		await conn.reply(m.chat, `👋 Kamu di Blockir karena menelepon *Bot*\n\nChat Owner *Bot* Suruh Buka Blokiran:\nhttps://wa.me/6287740530274`,wm + '\n\n' + run, m)
 		await delay(1000)
 		await this.updateBlockStatus(m.chat, "block")
 	}
