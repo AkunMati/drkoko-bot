072efe3472bfd8303fc774423b7351b91d68bcdc
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-10 02:12:17 +0700

    Update dl_fb.js

--- plugins/dl_fb.js
+++ plugins/dl_fb.js
@@ -1,21 +1,14 @@
 const { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
-
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
     if (!args[0]) throw `Use example / Gunakan ${usedPrefix}${command} https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`
-
+    m.react('⏱️')
     const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-
-    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, ` Nih kak `, m)
-
+    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `© TioXd`, m)
 }
-
 handler.help = ['facebook'].map(v => v + ' <url>')
-
 handler.tags = ['downloader']
-
 handler.command = /^((facebook|fb)(downloder|dl)?)$/i
-handler.premium = true
 handler.limit = true
-module.exports = handler
+handler.premium = true
 
+module.exports = handler
