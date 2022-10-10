e73e5827adf904dd385f464f0c63018a013be917
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-10 16:03:33 +0700

    Update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -1,46 +1,33 @@
-let axios = require('axios')
-let limit = 10
+let limit = 50
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  m.react('⏱️')
-  let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  let name = await conn.getName(m.sender)
-let wm = global.wm
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
-`.trim(), m)
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-  catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-`.trim(), m, false, {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+let handler = async(m, { conn, args, isPrems, isOwner }) => {
+    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+    let chat = db.data.chats[m.chat]
+    let server = (args[1] || servers[0]).toLowerCase()
+    let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
+    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+    conn.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : data.wait, m)
+    let _thumb = {}
+    try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } } catch (e) {}
+    m.reply(data.wait)
+    if (!isLimit) await conn.sendButtonVid(m.chat, dl_link, `*Title:* ${title}\n*Filesize:* ${filesizeF}`.trim(), wm, 'Audio', `.yta ${args}`, m, {
+  contextInfo: { mentionedJid: [m.sender],
+    externalAdReply :{
+    showAdAttribution: true,
+    mediaUrl: data.sc,
+    mediaType: 2,
+    description: data.deslink , 
+    title: run,
+    body: wm,
+    thumbnail: bg,
+    sourceUrl: data.sc
+     }}
+  })
+
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url>`)
+handler.help = ['ytmp4 <query>']
 handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = true
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
+handler.command = /^yt(v(idi?e?o)?|mp4)?$/i
 handler.limit = true
-
 module.exports = handler
