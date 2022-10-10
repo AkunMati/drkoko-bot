345091bb6cfd0320d8e1734a04e509496c7583e9
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-10 18:07:40 +0700

    Update short-short.js

--- plugins/short-short.js
+++ plugins/short-short.js
@@ -61,9 +61,9 @@ if (args[1] == "ouo") {
 	m.reply(pros).then(_=> conn.reply(m.chat, `${tesk}${ouo.result}`,m))
 	}
 }
-handler.help = ['short2 <url> <type>']
+handler.help = ['short <url> <type>']
 handler.tags = ['shortlink']
-handler.command = /^(short2(url)?)$/i
+handler.command = /^(short(url)?)$/i
 
 module.exports = handler
 
