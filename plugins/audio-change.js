25810cf8aa578e391e2b036fc326577dba68ff37
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-10 11:35:43 +0700

    Update audio-change.js

--- plugins/audio-change.js
+++ plugins/audio-change.js
@@ -40,7 +40,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     }
 }
 handler.help = ['bass', 'blown', 'deep', 'earrape', 'fast', 'fat', 'nightcore', 'reverse', 'robot', 'slow', 'smooth', 'tupai', 'vibra'].map(v => v + ' [vn]')
-handler.tags = ['Pengubah Suara']
+handler.tags = ['change']
 handler.command = /^(bass|blown|deep|earrape|fas?t|nightcore|reverse|robot|slow|smooth|tupai|squirrel|chipmunk|vibra)$/i
 
 module.exports = handler
