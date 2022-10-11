e24a11d7f956f6f02c679dfe2ae7a1610cfeb367
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-11 10:25:30 +0700

    Create logo-kaneki.js

---
+++ plugins/logo-kaneki.js
@@ -0,0 +1,14 @@
+//import fetch from 'node-fetch'
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   let response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Parameter'
+  m.reply('Proses...')
+  let res = `https://ziy.herokuapp.com/api/maker/kaneki?nama=${response[0]}&apikey=xZiyy`
+  conn.sendFile(m.chat, res, 'kaneki.jpg', `*Sudah Jadi Kak>_<*`, m, false)
+}
+handler.help = ['logokaneki'].map(v => v + ' <text>')
+handler.tags = ['logo']
+handler.command = /^(logokaneki)$/i
+handler.limit = true
+module.exports = handler
