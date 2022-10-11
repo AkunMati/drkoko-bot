3941acf9d05fc31716ce6bc4d09a097cdfc3a95c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-11 11:01:32 +0700

    Create logo-loli.js

---
+++ plugins/logo-loli.js
@@ -0,0 +1,18 @@
+//import fetch from 'node-fetch'
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+let response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Textnya Dungu!'
+  m.react('⏱️')
+  m.reply('Proses...')
+  let res = `https://ziy.herokuapp.com/api/maker/lolimaker?nama=${response[0]}&apikey=xZiyy`
+  conn.sendFile(m.chat, res, 'sadboy.jpg', `*Sudah Jadi Kak>_<*`, m, false)
+}
+handler.help = ['logololi'].map(v => v + ' <text>')
+handler.tags = ['logo']
+handler.command = /^(logololi)$/i
+handler.register = false
+
+handler.limit = true
+
+module.exports = handler
