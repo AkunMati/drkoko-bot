bb6ee43155b2e5d352190f83bd092c9f25e2db7d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-11 11:20:55 +0700

    Update logo-neko.js

--- plugins/logo-neko.js
+++ plugins/logo-neko.js
@@ -2,8 +2,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
-  m.reply('Proses...')
+  if (!args[0]) throw 'Masukkan Textnya Dungu!'
+  m.react('⏱️')
+  m.reply('Sedang Diproses...')
   let res = `https://ziy.herokuapp.com/api/maker/girlneko?text1=${response[0]}&text2=${response[1]}&apikey=xZiyy`
   conn.sendButtonImg(m.chat, res, 'neko.jpg', `Sudah Jadi`, wm, `TERIMA KASIH GANTENG:v`, `terimakasih` m, false)
 }
