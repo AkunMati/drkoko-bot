82f8645f363f18062d685a8c1b60f7f8a94eca2d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-13 19:33:26 +0700

    Update _New-_antiCall.js

--- plugins/_New-_antiCall.js
+++ plugins/_New-_antiCall.js
@@ -18,7 +18,7 @@ handler.all = async function (m) {
 	if(!setting.anticall) return 
 	
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await conn.sendButton(m.chat, `👋 Kamu di Blockir Karena Menelepon *Bot*\nHubungi Owner *Bot* Suruh Buka Blokiran!\n\nNomor Owner:\nwa.me/6283863727744`, wm + '\n\n' + run, [['KOKO','.owner']], null)
+		await conn.sendButton(m.chat, `👋 Kamu di Blockir Karena Menelepon *Bot*\nHubungi Owner *Bot* Suruh Buka Blokiran!\n\nNomor Owner:\nwa.me/6283863727744`, wm + '\n\n' + run, `KOKO`, `.owner`, null)
 		await delay(1000)
 		await this.updateBlockStatus(m.chat, "block")
 	}
