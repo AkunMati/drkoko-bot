dd6edba8adc68d3239c7e5823c63ddc7dfb6240d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-13 21:03:21 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,18 +1,17 @@
 let fs = require('fs')
 let handler = async (m, { conn, args, command }) => {
-let img = `https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg`,
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButtonImg(m.chat, img, `Total Fitur Bot Saat ini: ${totalf}\n`, wm + '\n\n' + run, [['THANKS KOKO GANTENG:v','terimakasih']], m, {
+ await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`, wm, giflogo, [['THANKS KOKO GANTENG:v','terimakasih']], m, {
 contextInfo: { externalAdReply :{
-                        mediaUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ',
+                        mediaUrl: data.linkig
                         mediaType: 2,
                         description: 'Kamu Sayang Aku Kan?',
                         title: run,
                         body: 'Total Cintaku Padamu',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ'
+                        sourceUrl: data.linkig
                       }}
 })
 }
