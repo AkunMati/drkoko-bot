91109bcfd9c05c45954c2acc7f7fe9a3ea87db0d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-13 18:14:41 +0700

    Update t-totalFitur.js

--- plugins/t-totalFitur.js
+++ plugins/t-totalFitur.js
@@ -1,9 +1,22 @@
-let handler = async (m, { conn }) => {
-  let lang = db.data.users[m.sender].lang
-  let tot = Object.values(global.plugins).filter(p => !p.disabled).map(p => Array.isArray(p.command) ? p.command : [p.command]).length
-  let total = await conn.translate(lang, 'Total fitur ' + tot).catch((_) => 'Total fitur ' + tot)
-  m.reply(total)
+import fs from 'fs'
+let handler = async (m, { conn, args, command }) => {
+let totalf = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+ await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`, wm + '\n\n' + run, fake, [['KOKO','.owner']], m, {
+contextInfo: { externalAdReply :{
+                        mediaUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ',
+                        mediaType: 2,
+                        description: 'Total Cintaku Padamu',
+                        title: run,
+                        body: wm,          previewType: 0,
+                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        sourceUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ'
+                      }}
+})
 }
+
+
 handler.help = ['totalfitur']
 handler.tags = ['main']
 handler.command = /^(total)?fitur$/i
