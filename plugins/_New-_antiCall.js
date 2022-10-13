8ea30399c6b4fcd7120fac6014bac2cd7b8d3724
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-13 08:31:00 +0700

    Update _New-_antiCall.js

--- plugins/_New-_antiCall.js
+++ plugins/_New-_antiCall.js
@@ -24,17 +24,4 @@ handler.all = async function (m) {
 	}
 }
 
-
-if (m.fromMe && m.isBaileys) return !0
-	let text;
-	let setting = global.db.data.settings[this.user.jid]
-	if(!setting.anticall) return 
-	
-	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await conn.sendButton(m.chat, `👋 Kamu di Blockir karena menelepon *Bot*wm + '\n\n' + run, `Anti Call`, `.owner`, null)
-		await delay(1000)
-		await this.updateBlockStatus(m.chat, "block")
-	}
-}
-
 module.exports = handler
