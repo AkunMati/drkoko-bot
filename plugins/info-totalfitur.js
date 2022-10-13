3104ff74e9d1cfbd4f3cbb819c8d613f3140a915
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-13 18:23:12 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,11 +1,24 @@
+let fs = require('fs')
 let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `Total Fitur saat ini: ${totalf}`,m)
+ await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`, wm + '\n\n' + run, fake, [['KOKO','.owner']], m, {
+contextInfo: { externalAdReply :{
+                        mediaUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ',
+                        mediaType: 2,
+                        description: 'Kamu Sayang Aku Kan?',
+                        title: run,
+                        body: 'Total Cintaku Padamu',          previewType: 0,
+                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        sourceUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ'
+                      }}
+})
 }
 
+
 handler.help = ['totalfitur']
-handler.tags = ['info']
-handler.command = ['totalfitur']
+handler.tags = ['main']
+handler.command = /^(total)?fitur$/i
+
 module.exports = handler
