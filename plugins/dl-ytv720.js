23332fe7cfcb282787d095b509a46dcd998bd03c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-15 10:20:26 +0700

    Create dl-ytv720.js

---
+++ plugins/dl-ytv720.js
@@ -0,0 +1,19 @@
+let xfar = require('xfarr-api')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text }) => {
+    if (!text) throw 'Masukkan Link\n\nContoh: .ytv720 https://youtube.com/xxxxxx'
+  let res = await xfar.Youtube(text)
+m.react('⏱️')
+m.reply('*Tunggu Sebentar...*')
+conn.sendFile(m.chat,res.medias[2].url, '', `Youtube Downloader
+720p
+jika masih blur, berarti videonya emang ngeblur dari youtubenya
+Atau bisa jadi faktor *MUKA*`, m)
+
+}
+handler.help = ['ytv720 <url>']
+handler.tags = ['internet']
+handler.command = /^ytv720$/i
+handler.premium = true
+handler.limit = true
+module.exports = handler
