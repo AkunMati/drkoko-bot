b7e0bc09fe6363466ab5377acaf4ebdfc088e711
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-18 21:16:54 +0700

    Update g-linkgroup.js

--- plugins/g-linkgroup.js
+++ plugins/g-linkgroup.js
@@ -1,15 +1,22 @@
 const { areJidsSameUser } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
+    let pp = './src/avatar_contact.png'
+    try {
+        pp = await conn.profilePictureUrl(m.chat, 'image')
+    } catch (e) {
+    } finally {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
     if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa dibuka di grup'
     let groupMetadata = await conn.groupMetadata(group)
-    if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants is not defined :('
-    let bot = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!bot) throw 'Gw gk ada di grup itu 🗿'
-    if (!bot.admin) throw 'Gw bukan atmin bg 🗿'
-    await conn.sendHydrated(m.chat, `*${htki} LINK GROUP ${htka}*`, run, null, 'https://www.whatsapp.com/otp/copy/'+'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), 'S A L I N', swb, wm, 'SEWA BOT', '.sewain', 'KOKO', '.owner', m)
+    if (!groupMetadata) throw 'grup tidak diketahui!'
+    if (!('participants' in groupMetadata)) throw 'peserta grup tidak diketahui!'
+    let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
+    if (!me) throw 'Aku tidak ada di grup itu :('
+    if (!me.admin) throw 'Aku bukan admin T_T'
+    let name = await conn.getName(group)
+    conn.send2ButtonImg(m.chat, pp, `*Link Group:* ${name}\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(group), wm, 'RULESNYA', '.rules', 'KOKO', '.owner', m)
+  }
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
