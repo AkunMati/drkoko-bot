f1d0dcdeec6f518dde9893b8b9479d29e61f534e
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-18 20:31:55 +0700

    Update g-linkgroup.js

--- plugins/g-linkgroup.js
+++ plugins/g-linkgroup.js
@@ -6,13 +6,10 @@ let handler = async (m, { conn, args }) => {
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
     if (!('participants' in groupMetadata)) throw 'participants is not defined :('
-    let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw 'Gw gk ada di grup itu 🗿'
-    if (!me.admin) throw 'Gw bukan atmin bg 🗿'
-    await conn.sendHydrated(m.chat, `*${htki} LINK GROUP ${htka}*`, run, null, 'https://www.whatsapp.com/otp/copy/'+'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), 'S A L I N', swb, wm, [
-      ['SEWA BOT', '/sewain'],
-      ['KOKO', '/owner']
-    ], null)
+    let bot = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
+    if (!bot) throw 'Gw gk ada di grup itu 🗿'
+    if (!bot.admin) throw 'Gw bukan atmin bg 🗿'
+    await conn.sendHydrated(m.chat, `*${htki} LINK GROUP ${htka}*`, run, null, 'https://www.whatsapp.com/otp/copy/'+'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), 'S A L I N', swb, wm, 'SEWA BOT', '.sewain', 'KOKO', '.owner', m)
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
