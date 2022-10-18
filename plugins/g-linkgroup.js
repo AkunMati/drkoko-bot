5e99028e795bf3b66d5189f3425f0583d2ca3f59
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-18 20:16:27 +0700

    Update g-linkgroup.js

--- plugins/g-linkgroup.js
+++ plugins/g-linkgroup.js
@@ -1,10 +1,5 @@
 const { areJidsSameUser } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
-    let pp = './src/avatar_contact.png'
-    try {
-        pp = await conn.profilePictureUrl(m.chat, 'image')
-    } catch (e) {
-    } finally {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
     if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa dibuka di grup'
@@ -14,11 +9,10 @@ let handler = async (m, { conn, args }) => {
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
     if (!me) throw 'Gw gk ada di grup itu 🗿'
     if (!me.admin) throw 'Gw bukan atmin bg 🗿'
-    await conn.sendHydrated(m.chat, pp, `*${htki} LINK GROUP ${htka}*`, run, null, 'https://www.whatsapp.com/otp/copy/'+'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), 'S A L I N', swb, wm, [
+    await conn.sendHydrated(m.chat, `*${htki} LINK GROUP ${htka}*`, run, null, 'https://www.whatsapp.com/otp/copy/'+'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), 'S A L I N', swb, wm, [
       ['SEWA BOT', '/sewain'],
       ['KOKO', '/owner']
     ], null)
-  }
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
