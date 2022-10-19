3ce2bd59ffbd901ae6ccd2c3ac2a3f23c50a7551
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-19 18:39:29 +0700

    Update gm-gombal-ans.js

--- plugins/gm-gombal-ans.js
+++ plugins/gm-gombal-ans.js
@@ -7,25 +7,25 @@ handler.before = async function (m) {
         return !0
     this.tebakgombal = this.tebakgombal ? this.tebakgombal : {}
     if (!(id in this.tebakgombal))
-        return conn.sendButton(m.chat, 'Soal itu telah berakhir', author, null, buttontebakgombal, m)
+        return conn.sendButt(m.chat, 'Soal itu telah berakhir', author, null, buttontebakgombal, m)
     if (m.quoted.id == this.tebakgombal[id][0].id) {
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (isSurrender) {
             clearTimeout(this.tebakgombal[id][3])
             delete this.tebakgombal[id]
-            return conn.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttontebakgombal, m)
+            return conn.sendButt(m.chat, '*Yah Menyerah :( !*', author, null, buttontebakgombal, m)
         }
         let json = JSON.parse(JSON.stringify(this.tebakgombal[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakgombal[id][2]
-            conn.sendButton(m.chat, `*Benar!*\n+${this.tebakgombal[id][2]} XP`, author, null, buttontebakgombal, m)
+            conn.sendButt(m.chat, `*Benar!*\n+${this.tebakgombal[id][2]} XP`, author, null, buttontebakgombal, m)
             clearTimeout(this.tebakgombal[id][3])
             delete this.tebakgombal[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
             m.reply(`*Dikit Lagi!*`)
         else
-            conn.sendButton(m.chat, `*Salah!*`, author, null, [
+            conn.sendButt(m.chat, `*Salah!*`, author, null, [
                 ['Hint', '.hgom'],
                 ['Nyerah', 'menyerah']
             ], m)
