f22e3a3563e7ee367734d3266af338c851991bb4
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-19 12:22:54 +0700

    Update xp-unregister.js

--- plugins/xp-unregister.js
+++ plugins/xp-unregister.js
@@ -1,15 +1,14 @@
 const { createHash } = require('crypto')
-let handler = async function (m, { args, usedPrefix }) {
-  if (!args[0]) throw `Serial Number kosong\nKetik *${usedPrefix}serial* untuk mendapatkan kode SN anda`
-  let user = db.data.users[m.sender]
+et handler = async function (m, { args }) {
+  if (!args[0]) throw '📮Serial Number kosong'
+  let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+  if (args[0] !== sn) throw '🚫Serial Number salah'
   user.registered = false
-  m.reply(`Unreg berhasil!`)
+  m.reply('📛Kamu Berhasil keluar dari database\n\nᵈʳкσкσ-ᴍᴅ×፝֟͜×')
 }
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
 handler.tags = ['xp']
-
 handler.command = /^unreg(ister)?$/i
 handler.register = true
 
