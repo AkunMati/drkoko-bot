03f267ea2f929b10a3983ccf160e2f1c0d1cc3c5
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-19 10:39:16 +0700

    Update fun-cersex.js

--- plugins/fun-cersex.js
+++ plugins/fun-cersex.js
@@ -1,20 +1,20 @@
+const fetch = require('node-fetch')
 let axios = require("axios");
-let handler = async(m, { conn, text }) => {
-
-    //if (!text) return conn.reply(m.chat, 'Masukan parameter, Contoh *#lirik my love*', m)
-
-  await m.reply('Sedang di proses kak:b')
-	axios.get(`https://api-xcoders.xyz/api/random/cersex?apikey=xcoders`).then ((res) => {
-	 	let hasil = `~> *Judul* : ${res.data.result.title}\n\n _${res.data.result.cerita}_`
-
-    conn.sendFile(m.chat, res.data.result.thumbnail, 'carsex.jpeg', hasil, m)
-	})
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+        let json = await fetch(`https://api-xcoders.xyz/api/random/cersex?apikey=${global.xckey}`)
+        let jsons = await json.json()
+        let x = jsons.result
+        let caption = `*⎔┉━「 Cersex 」━┉⎔*
+🤠 *Query* : ${x.title}
+ ${x.date}
+ 
+ ${x.cerita}
+`
+        await conn.sendButton(m.chat, caption, wm, x.thumbnail, `cersex`, `.cersex`, m)
 }
 handler.help = ['cersex']
-handler.tags = ['fun']
-handler.command = /^cersex$/i
-handler.register = true
+handler.tags = ['nsfw']
+handler.command = /^(cersex)$/i
 handler.premium = true
-
 
 module.exports = handler
