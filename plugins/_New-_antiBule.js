63bd55e706e938078000cb0b3a60ab57882fd60c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-21 11:46:44 +0700

    Create _New-_antiBule.js

---
+++ plugins/_New-_antiBule.js
@@ -0,0 +1,15 @@
+handler.before = async {m, { isAdmin, isBotAdmin }) {
+    let name = await conn.getName(m.sender)
+    let chat = global.db.data.chats[m.chat]
+    let user = global.db.data.users[m.sender]
+    let caption = `👋 Anti Bule ${name} @${m.sender.split("@")[0]}, Thanks!`.trim()
+   if (chat.antibule) {
+   if (!m.sender.startsWith('62' || '1')) {
+   	user.banned = true
+   	this.sendButt(m.chat, caption, author, null, `${imgr + 'Anti Bule'}`, [['Disable Anti Bule', '.off antibule']], m, { mentions: this.parseMention(caption) })
+   	return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+   }
+  }
+ }
+
+module.exports = handler
