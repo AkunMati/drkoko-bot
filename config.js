a1779bb8fde6bf84bf7ef8e59ccfb241153c6fb9
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-21 13:10:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.APIKeys = { // APIKey Here
 }
 global.packname = data.namabot
 global.author = data.owner
-global.wm = 'ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜× × Qᴜᴇᴇ፝֟፝֟ɴ×፝֟͜×'
+global.wm = '©ᵈʳкσкσ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜× × Qᴜᴇᴇ፝֟፝֟ɴ×፝֟͜×'
 
 global.asing = `*Ciee Orang Asing Join, Dadah Saya Kick👋*`
 global.wait = `*Tunggu Sebentar Dungu!*`
