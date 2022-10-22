b73a99a96b49fa2581a3aeac19fceef83f20d681
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-22 13:41:35 +0700

    Update _simiAutoVn.js

--- plugins/_simiAutoVn.js
+++ plugins/_simiAutoVn.js
@@ -5,6 +5,8 @@ if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
     if (chat.simiautoVn) {
+    if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+     if (!m.text) return
     let sim = await fetch(`https://api.simsimi.net/v2/?text=${m.text}&lc=id`)
     let res = await sim.json()
         let tts = `https://hadi-api.herokuapp.com/api/tts?language=id&text=${res.success}`
