43bde536936e5a66cb9094b6f349eb6c7bce991a
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-22 11:45:46 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -1,4 +1,6 @@
-let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
+let img = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg'
+
+let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, img, `
 ╭─「 Donasi • Dana 」
 │ • MyXL [087832147584]
 │ • Gopay  [087832147584]
@@ -6,12 +8,12 @@ let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 │ • Saweria  [https://saweria.co/kokopangeran]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6287740530274
+│ > Ingin donasi? Wa.me/6283863727744
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
 ╰────
-`.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
+`.trim(), wm, '👦KOKO', usedPrefix + 'owner', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['about']
 handler.command = /^dona(te|si)$/i
