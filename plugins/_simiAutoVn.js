87c3964937a0ce7dd5e1b0f823e0179f18b957d0
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-22 19:30:09 +0700

    Update _simiAutoVn.js

--- plugins/_simiAutoVn.js
+++ plugins/_simiAutoVn.js
@@ -1,18 +1,17 @@
 const fetch = require('node-fetch')
 let handler = m => m
 handler.before = async function (m, { isAdmin, isBotAdmin }) {
-if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
-    if (chat.simiautoVn && !chat.isBanned ) {
-    if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-     if (!m.text) return
+    if (chat.simiautoVn) {
     let sim = await fetch(`https://api.simsimi.net/v2/?text=${m.text}&lc=id`)
     let res = await sim.json()
         let tts = `https://hadi-api.herokuapp.com/api/tts?language=id&text=${res.success}`
         conn.sendMessage(m.chat, { audio: { url: tts }, mimetype: 'audio/mp4' })
     }
-   return true
 }
 
 module.exports = handler
