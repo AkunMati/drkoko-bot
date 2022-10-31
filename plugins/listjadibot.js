381efd8c8fc31045903005d2698f98e17815d8b5
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-01 02:28:32 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -2,7 +2,8 @@ let handler = async (m, { usedPrefix }) => {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
   m.reply(users.map(v => 'wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
-handler.command = handler.help = ['listjadibot']
+handler.command = /^(listjadibot)$/i
+handler.help = ['listjadibot']
 handler.tags = ['jadibot']
 
 module.exports = handler
