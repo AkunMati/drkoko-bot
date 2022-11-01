977946e7b8192f892049f84a3b31b904d809d7af
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-02 04:08:11 +0700

    Update main-costume.js

--- plugins/main-costume.js
+++ plugins/main-costume.js
@@ -3,7 +3,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text, args}) => {
 
 let [jaw1, jaw2, per] = text.split("|")
-await conn.sendButton(m.chat, per, '©Undefined Bot', {"button[0]": `${jaw1}`, "row[0]": `${args[6]}`, "button[1]": `${jaw2}`, "row[1]": `${args[8]}`}) 
+await conn.sendButt(m.chat, per, '©Undefined Bot', {"button[0]": `${jaw1}`, "row[0]": `${args[6]}`, "button[1]": `${jaw2}`, "row[1]": `${args[8]}`}) 
 }
 handler.help = ['custombutton jawaban1 | jawaban2 | pertanyaan']
 handler.tags = ['main']
