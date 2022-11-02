21143521572a9cfaa8cbbff81680f9db24fcd4c6
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-02 10:23:59 +0700

    Update g-caripacar.js

--- plugins/g-caripacar.js
+++ plugins/g-caripacar.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, participants, command, usedPrefix }) => {
     else orang = member[Math.floor(Math.random() * member.length)]
     let jawab = `Hei @${orang.replace(/@.+/, '')} Mau Gak Jadi Pacar Gw? 😌`.trim()
     let mentionedJid = [orang]
-    await conn.sendButt(m.chat, jawab, '📮 Silahkan Cari Pacar Lagi', `Cari Pacar`, usedPrefix + command, m, { contextInfo: { mentionedJid } })
+    await conn.sendButton(m.chat, jawab, '📮 Silahkan Cari Pacar Lagi', `Cari Pacar`, usedPrefix + command, m, { contextInfo: { mentionedJid } })
 }
 handler.help = ['caripacar']
 handler.tags = ['fun']
