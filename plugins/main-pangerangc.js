39d734c9240e7fe709699371e185aa81ce6c16e1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-02 10:19:47 +0700

    Update main-pangerangc.js

--- plugins/main-pangerangc.js
+++ plugins/main-pangerangc.js
@@ -1,11 +1,11 @@
-let handler = async m => 
-
-m.reply(`
+let handler = async (m, { conn, args, command }) => {
+let img = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg'
+conn.sendButtonImg(m.chat, img, `
 ╰┈➤ ❝ *GC BOT* ❞
 ✤ *ʙᴏᴛ ʙʏ ᴘᴀɴɢᴇʀᴀɴ ᴍᴅ*
 _*LINK:*_ https://chat.whatsapp.com/IqFfoXcdh7UFyPoQTpKoQp
 (ɴ) sᴇʙᴇʟᴜᴍ ᴊᴏɪɴ ᴜsᴀʜᴀɪɴ ʙᴀᴄᴀ ᴅᴇsᴋ ᴅᴜʟᴜ ʏᴀ, ᴛʜᴀɴᴋʏᴏᴜᴜ
-`.trim()) // Tambah sendiri kalo mau
+`, wm, 'TERIMAKASIH KOKO GANTENG:v', 'terimakasih', m) // Tambah sendiri kalo mau
 handler.help = ['pangerangc']
 handler.tags = ['main']
 handler.command = /^pangeran(gc|group)$/i
