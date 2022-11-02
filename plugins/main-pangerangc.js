e2f26c0a689ba51ca41d12b6c64f86e5ea54a823
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-02 16:04:33 +0700

    Update main-pangerangc.js

--- plugins/main-pangerangc.js
+++ plugins/main-pangerangc.js
@@ -1,13 +1,32 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-let img = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg'
-conn.sendButtonImg(m.chat, img, `
-╰┈➤ ❝ *GC BOT* ❞
-✤ *ʙᴏᴛ ʙʏ ᴘᴀɴɢᴇʀᴀɴ ᴍᴅ*
-_*LINK:*_ https://chat.whatsapp.com/IqFfoXcdh7UFyPoQTpKoQp
-(ɴ) sᴇʙᴇʟᴜᴍ ᴊᴏɪɴ ᴜsᴀʜᴀɪɴ ʙᴀᴄᴀ ᴅᴇsᴋ ᴅᴜʟᴜ ʏᴀ, ᴛʜᴀɴᴋʏᴏᴜᴜ
-`, wm, 'TERIMAKASIH KOKO GANTENG:v', 'terimakasih', m) // Tambah sendiri kalo mau
-handler.help = ['pangerangc']
-handler.tags = ['main']
-handler.command = /^pangeran(gc|group)$/i
+const fetch = require('node-fetch')
+let teks = 'gcbot'
+let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch('https://telegra.ph/file/c6d47e0aa9dca2263bee9.png'+ teks)).buffer(), `
+*𝐇𝐚𝐥𝐨 𝐁𝐚𝐧𝐡 𝐉𝐚𝐧𝐠𝐚𝐧 𝐋𝐮𝐩𝐚 𝐉𝐨𝐢𝐧 && 𝐁𝐚𝐜𝐚 𝐑𝐮𝐥𝐞𝐬❗*
+╔═══════════════════
+║ _*Group All*_ ᵈʳкσкσ-ᴍᴅ×፝֟͜×
+╠═══════════════════
+║➸ GROUP INTI YUNI:
+║ https://chat.whatsapp.com/DsLuDkm9h8nDdgtTEKEW49
+║➸ 𝐆𝐫𝐨𝐮𝐩 𝐁𝐨𝐭 1:
+║ https://chat.whatsapp.com/IqFfoXcdh7UFyPoQTpKoQp
+║➸ 𝐆𝐫𝐨𝐮𝐩 𝐁𝐨𝐭 2:
+║ https://chat.whatsapp.com/LhpFRMqsKkn6nrCCf1He9R
+╚════════════════════
+╔════════════════════
+║ *RULES GROUP BOT*
+╠════════════════════
+║𝐍𝐨 𝐒𝐡𝐚𝐫𝐞 18+
+║𝐍𝐨 𝐬𝐡𝐚𝐫𝐞 𝐋𝐢𝐧𝐤
+║𝐍𝐨 𝐒𝐡𝐚𝐫𝐞 𝐕𝐢𝐫𝐮𝐬 
+║𝐍𝐨 𝐒𝐩𝐚𝐦 𝐂𝐨𝐦𝐦𝐚𝐧𝐝
+║𝐃𝐢𝐥𝐚𝐫𝐚𝐧𝐠 𝐓𝐞𝐥𝐟𝐨𝐧 𝐁𝐨𝐭
+║𝐉𝐚𝐧𝐠𝐚𝐧 𝐋𝐮𝐩𝐚 𝐁𝐞𝐫 𝐄𝐭𝐢𝐤𝐚
+║𝐃𝐢𝐥𝐚𝐫𝐚𝐧𝐠 𝐇𝐢𝐧𝐚 𝐁𝐨𝐭
+║𝐌𝐚𝐮 𝐒𝐞𝐰𝐚? 𝐊𝐞𝐭𝐢𝐤 .owner
+╚════════════════════
+`.trim(), wm, '😳KOKO', '.owner', 'MENU✔️', '.menu')
+handler.help = ['gcbot']
+handler.tags = ['info']
+handler.command = /^gc(bot|groupbot)$/i
 
 module.exports = handler
