93a68c7e44f35081ffe7a5c2efa141d2b85af6fa
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-04 02:30:06 +0700

    Update _antibot.js

--- plugins/_antibot.js
+++ plugins/_antibot.js
@@ -1,29 +1,23 @@
+// DETECTOR BOT
 let handler = m => m
 let imgr = pickRandom(flaaa)
 
 handler.before = async (m, { isAdmin, isBotAdmin }) => {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
     let name = await conn.getName(m.sender)
     let chat = global.db.data.chats[m.chat]
     let user = global.db.data.users[m.sender]
-    let caption = `👋 Anti Bule ${name} @${m.sender.split("@")[0]}, Thanks!`.trim()
+    let caption = `👋 Anti Bot ${name} @${m.sender.split("@")[0]}, Tidak Bole Ada Bot Lain Di Group Ini. Thanks!`.trim()
    if (chat.antibot) {
+   if (isAdmin) return m.reply('*Eh Sorry You Admins, You Will Not Be Kicked. hehe..*')
+   if (!isBotAdmin) return m.reply('*Bots are Not Admins, How Can They Kick People _-*')
    if (up.key.id.startsWith('BAE5') && !up.key.fromMe) {
    	user.banned = true
    	this.sendButt(m.chat, caption, author, null, `${imgr + 'Anti Bot'}`, [['Disable Anti Bule', '.off antibot']], m, { mentions: this.parseMention(caption) })
+        await conn.sendMessage(m.chat, { delete: m.key })
    	return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    }
   }
  }
 
 module.exports = handler
-
-// DETECTOR BOT
-			if (conn.db.data.chat[m.chat].antibot) {
-				if (up.key.id.startsWith('BAE5') && !up.key.fromMe) {
-					rep('[ ANTI BOT ]\ngroup ini dilengkapi dengan anti bot\nanda melanggar larangan bot\nAnda berhak di kick');
-					if (isAdmin) throw 'Maaf Kamu admin ternyata'
-					if (m.isOwn) throw 'Oh tidak, kamu ownerku';
-					if (!isBotAdmin) throw 'Oh tidak, Bot not admin';
-					kik();
-				}
-			}
