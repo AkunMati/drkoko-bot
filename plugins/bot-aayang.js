efbb0b7709bccc1e0b767499e0b658f573f66a62
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-05 05:23:22 +0700

    Create bot-aayang.js

---
+++ plugins/bot-aayang.js
@@ -0,0 +1,21 @@
+let { sticker } = require('../lib/sticker.js')
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
+    
+}
+
+handler.customPrefix = /^(Aayang)$/i
+handler.command = new RegExp
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+let stikerhuuu = [
+ "https://telegra.ph/file/00a0798b6decd4e18d681.png",
+]
