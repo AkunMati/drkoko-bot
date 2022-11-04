b892e943f824d9c9ea630cce2a49b1c0cdabc9e1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-05 04:26:41 +0700

    Update g-caripacar.js

--- plugins/g-caripacar.js
+++ plugins/g-caripacar.js
@@ -1,11 +1,12 @@
+let imgr = flaaa.getRandom()
 let handler = async (m, { conn, participants, command, usedPrefix }) => {
     let member = participants.map(u => u.id)
     let orang
     if (/ku/i.test(command)) orang = m.sender
     else orang = member[Math.floor(Math.random() * member.length)]
-    let jawab = `Hei @${orang.replace(/@.+/, '')} Mau Gak Jadi Pacar Gw? 😌`.trim()
+    let jawab = `Ehh @${orang.replace(/@.+/, '')} Mau Gak Jadi Pacar Gw? 😌`.trim()
     let mentionedJid = [orang]
-    await conn.sendButton(m.chat, jawab, '📮 Silahkan Cari Pacar Lagi', `Cari Pacar`, usedPrefix + command, m, { contextInfo: { mentionedJid } })
+    await conn.sendButton(m.chat, jawab, '📮Silahkan Cari Pacar Lagi📮', wm + '\n\n' + run, `${imgr + 'Cari Pacar'}`, [['Cari Lagi?','.caripacar']], m, { contextInfo: { mentionedJid } })
 }
 handler.help = ['caripacar']
 handler.tags = ['fun']
