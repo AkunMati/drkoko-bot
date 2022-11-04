76333f1419699ebbc69dfa2e93988df45fcfbe4d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-05 04:16:11 +0700

    Update fun-ngeweyok.js

--- plugins/fun-ngeweyok.js
+++ plugins/fun-ngeweyok.js
@@ -1,11 +1,12 @@
+let imgr = flaaa.getRandom()
 let handler = async (m, { conn, participants, command, usedPrefix }) => {
     let member = participants.map(u => u.id)
     let orang
     if (/ku/i.test(command)) orang = m.sender
     else orang = member[Math.floor(Math.random() * member.length)]
-    let jawab = `Aku Sange Nih, Ngewe yuk @${orang.replace(/@.+/, '')} 🤤`.trim()
+    let jawab = `Aku Sange Nih, Ngewe yuk @${orang.replace(/@.+/, '')} 🥵🤤`.trim()
     let mentionedJid = [orang]
-    await conn.sendBut(m.chat, jawab, '📮 Silahkan Klik Button dibawah ini', `Ahh ahh🥵`, usedPrefix + command, m, { contextInfo: { mentionedJid } })
+    await conn.sendButt(m.chat, jawab, '📮Silahkan Klik Button Dibawah Ini📮', wm + '\n\n' + run, `${imgr + 'Ngewe Yok'}`, [['Ahh Ahh🥵','.ngeweyok']], m, { contextInfo: { mentionedJid } })
 }
 handler.help = ['ngeweyok']
 handler.tags = ['fun']
