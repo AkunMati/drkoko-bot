b70cf070d0c1af571999e7a07eb9b971f1db2e31
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-05 01:54:37 +0700

    Update o-update.js

--- plugins/o-update.js
+++ plugins/o-update.js
@@ -1,21 +1,24 @@
-const { execSync } = require('child_process')
-const { readdirSync } = require('fs')
-const { format } = require('util')
-
+let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
-  try {
-    let stdout = execSync('git remote set-url origin ' + set.repo + ' && git pull' + (isROwner && text ? ' ' + text : ''))
-    if (isROwner) readdirSync('plugins').map(v => global.reload('', v))
-    let hasil = stdout.toString()
-    m.react('⚡').then(_=> 
-       conn.sendButton(m.chat, hasil, '                Node Test or Restart Bot?', 0, [['Restart', '.restart'], ['Node Test', '$ node test']], m)
-    )
-    } catch (e) {
-    conn.reply(m.chat, format(e), m)
+  if (global.conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git remote set-url origin https://github.com/sadteams/bot-md.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
   }
 }
 handler.help = ['update']
-handler.tags = ['host']
-handler.command = /^(update|u)$/i //sedia payung sebelum hujan meteor 
-handler.rowner = true
+handler.tags = ['owner']
+handler.command = /^update$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
 module.exports = handler
