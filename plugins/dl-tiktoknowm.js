cab1fc8f3a5ec09beb9e9174108d495edd84e5db
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-06 01:52:19 +0700

    Update dl-tiktoknowm.js

--- plugins/dl-tiktoknowm.js
+++ plugins/dl-tiktoknowm.js
@@ -1,41 +1,29 @@
-const fetch = require('node-fetch')
-const axios = require('axios')
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-if (!args[0]) throw 'Masukkan Link'
+let fetch = require('node-fetch')
+let axios = require('axios')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
 m.react('⏱️')
-try {
-    let listSections = []
-	listSections.push(['No. ' + ++index, [
-          ['Metode A', usedPrefix + command + ' ' + args[0] + ' a', '\n⌚ *By:* ' + author],
-          ['Metode B', usedPrefix + command + ' ' + args[0] + ' b', '\n⌚ *By:* ' + author],
-          ['Metode C', usedPrefix + command + ' ' + args[0] + ' c', '\n⌚ *By:* ' + author]
-                  ]])
-        if (args[0]) return conn.sendList(m.chat, htki + ' 📺 Tiktok Search 🔎 ' + htka, `⚡ Silakan pilih Tiktok Search di tombol di bawah...\n*Teks yang anda kirim:* ${args[0]}\n\nKetik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi`, author, `☂️ Tiktok Search Disini ☂️`, listSections, m)
-
-if (args[1] == 'a') {
-  let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${global.lolkey}&url=${args[0]}`)
+let res = await fetch(API('lol', '/api/tiktok', { url: args[0] }, 'apikey'))
     let json = await res.json()
-    let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
-    conn.send2ButtonVid(m.chat, json.result.link, txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, m)
-    }
-    if (args[1] == 'b') {
-    let res = await fetch(`https://api.lolhuman.xyz/api/tiktok2?apikey=${global.lolkey}&url=${args[0]}`)
-    let json = await res.json()
-    let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
-    conn.send2ButtonVid(m.chat, json.result.link, txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, m)
-    }
-    if (args[1] == 'c') {
-    let res = await fetch(`https://api.lolhuman.xyz/api/tiktok3?apikey=${global.lolkey}&url=${args[0]}`)
-    let json = await res.json()
-    let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
-    conn.send2ButtonVid(m.chat, json.result.link, txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, m)
-    }
-    } catch (e) {
-    throw eror
-    }
+    
+    m.reply(data.wait)
+await conn.reply(m.chat, `Lain Kali Download Sendiri Memek!`, 0, {
+  contextInfo: { mentionedJid: [m.sender],
+    externalAdReply :{
+    mediaUrl: data.sc,
+    mediaType: 2,
+    description: data.deslink , 
+    title: run,
+    body: wm,
+    thumbnail: await(await fetch(img)).buffer(),
+    sourceUrl: data.sc
+     }}
+  })
+let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
+    await conn.sendButtonVid(m.chat, json.result.link, txt, wm, `Audio`, `.tiktokaudio ${args[0]}`, m)
 }
 handler.help = ['tiktoknowm'].map(v => v + ' <url>')
-handler.tags = ['downloader', 'premium']
+handler.tags = ['downloader']
 handler.command = /^(tt|tiktok)nowm(dl)?(download(er)?)?$/i
 handler.premium = true
 handler.limit = true
