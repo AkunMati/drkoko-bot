0612c24ade33d4439ee75fe8822f2efd3766feea
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-07 04:47:10 +0700

    Update and rename dl-tiktokaudio.js to dl-tiktok2.js

---
+++ plugins/dl-tiktok2.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Uhm..url nya mana?'
+m.reply('Tunggu sebentar...')
+let res = await fetch(`https://botcahx.ddns.net/api/dowloader/tikok?url=${args[0]}`)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+let { video, description, username } = json.result
+await conn.sendFile(m.chat, video, 'video.mp4', `
+🧏 Username: ${username}
+📋 Deskripsi: ${description}
+🔗 Url: ${video}
+`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+}
+
+handler.help = ['tiktok2 <url>']
+handler.tags = ['downloader']
+handler.command = /^(tiktok2)$/i
+handler.limit = true
+handler.premium = true
+module.exports = handler
