efa5c9a8d9e80432f7f4ee3b6cce73c4c4c3a88d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-06 22:17:57 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,35 +1,23 @@
-let fetch = require('node-fetch')
 let axios = require('axios')
-const { tiktok } = require('social_media_downloader')
-let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-if (!args[0]) throw 'Masukkan Link'
-m.react('⏱️')
-try {
-let p = await tiktok(args[0])
-    if (!p.link) throw 'Can\'t download video!'
-    let cap = `*「 T I K T O K 」*
-                 ████████▀▀▀████
-                 ████████────▀██
-                 ████████──█▄──█
-                 ███▀▀▀██──█████
-                 █▀──▄▄██──█████
-                 █──█████──█████
-                 █▄──▀▀▀──▄█████
-                 ███▄▄▄▄▄███████
-──────────────────────────
-*📛Nickname:* ${p.dev}
-*📒Description:* ${p.description}
-*Url:* ${p.url}
-`.trim()
-conn.send2ButtonVid(m.chat, p.link, cap, author, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, fake, adReply)
-} catch (e) {
-    throw eror
-    }
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    m.react('⏱️')
+    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    if (res.status != 200) throw res.message;
+    if (!res) throw res.message;
+    
+    let result = `⟐⟞⟚⟝⟮ *Title:* ⟯⟞⟚⟝⟐
+┇⟣⟪ ${res.title} ⟫⟢
+▥ ━┉┄┄┈┈ ►
+┇⟐⟞⟚⟝⟮ *Author* ⟯⟞⟚⟝⟐
+▥ ━┉┄┄┈┈ ►
+${res.author}
+◈ ━┉┈┄┈┈ ►`
+    conn.sendButtonVid(m.chat, res.video, result, '©ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×', `Audio`, `.gettt ${args[0]}`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^t(iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i
-//handler.premium = true
+handler.command = /^(tiktok|ttdl|tt|tiktokdl)$/i
+handler.premium = true
 handler.limit = true
-
 module.exports = handler
