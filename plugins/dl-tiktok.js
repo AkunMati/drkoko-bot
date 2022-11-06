e9a1dd0faee13a4ac9f7430ce1505676b4619257
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-07 02:54:33 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,23 +1,24 @@
-const axios = require('axios')
+const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
+let fetch = require('node-fetch')
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    m.react('⏱️')
-    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
-    
-    let result = `⟐⟞⟚⟝⟮ *Title:* ⟯⟞⟚⟝⟐
-┇⟣⟪ ${res.title} ⟫⟢
-▥ ━┉┄┄┈┈ ►
-┇⟐⟞⟚⟝⟮ *Author* ⟯⟞⟚⟝⟐
-▥ ━┉┄┄┈┈ ►
-${res.author}
-◈ ━┉┈┄┈┈ ►`
-    conn.sendButtonVid(m.chat, res.video, result, '©ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×', `⏩AUDIO`, `.tomp3`, m)
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+ m.react('⏱️')
+
+  const { author: { nickname }, video, description } = await tiktokdl(args[0])
+        .catch(async _ => await tiktokdlv2(args[0]))
+        .catch(async _ => await tiktokdlv3(args[0]))
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
+    if (!url) throw 'Can\'t download video!'
+    conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
+        url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
+        ['ᴅᴏɴᴀꜱɪ', `.donasi`],
+        ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`],
+        [null, null]], m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|ttdl|tt|tiktokdl)$/i
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 handler.premium = true
 handler.limit = true
 module.exports = handler
