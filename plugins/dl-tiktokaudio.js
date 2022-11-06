2114a49f1b756092073ff34449f530c47cb29f7c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-06 22:27:00 +0700

    Update dl-tiktokaudio.js

--- plugins/dl-tiktokaudio.js
+++ plugins/dl-tiktokaudio.js
@@ -2,6 +2,7 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+   m.react('⏱️')
     let res = (await axios.get(API('males', '/tiktok2', { url: args[0] } ))).data;
     if (res.status != 200) throw res.message;
     if (!res) throw res.message;
