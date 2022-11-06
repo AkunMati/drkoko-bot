1a3f120477545562071a6b5440be2196e2d95c74
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-07 04:17:48 +0700

    Update cecan.js

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -1,8 +1,8 @@
-const fetch = require('node-fetch')
+let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
 	let url = 'https://api.lolhuman.xyz/api/random/cecan?apikey=64333e1c050ced435defe154'
-	conn.sendButt(m.chat, '*–––| Sukses |–––*', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
+	conn.sendButtonImg(m.chat, '*–––| Sukses |–––*', wm, await(await fetch(url)).buffer(), '🔁Next🔁', `.${command}`,m)
 }
 handler.command = /^(cecan)$/i
 handler.tags = ['random']
