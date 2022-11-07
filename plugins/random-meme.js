260e49102fd110461fb724045b22e0a5c3a76659
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-08 00:43:39 +0700

    Create random-meme.js

---
+++ plugins/random-meme.js
@@ -0,0 +1,12 @@
+let axios = require("axios")
+let handler = async (m, {command, conn}) => {
+let res = await axios(`https://meme-api.herokuapp.com/gimme`)
+let json = res.data
+let url = json.url
+conn.sendButtonImg(m.chat, `*${command}*`.trim(), wm, url, '🔄 NEXT 🔄', `.meme`, m)
+}
+handler.help = ['meme']
+handler.tags = ['random']
+handler.command = /^(meme)$/i
+handler.limit = true
+module.exports = handler
