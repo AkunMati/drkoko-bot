b9db4bff7b6e8b198c135397bf2ce0a9cb219f2f
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-08 00:39:06 +0700

    Create random-gore.js

---
+++ plugins/random-gore.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+        let json = await fetch(`https://api-xcoders.xyz/api/random/gore?apikey=${global.xckey}`)
+        let jsons = await json.json()
+        let x = jsons.result
+        let caption = `*⎔┉━「 gore 」━┉⎔*
+🤠 *Query* : ${x.title}
+ ${x.authorname}
+ ${x.published}
+ ${x.author_url}
+ ${x.quality}
+ ${x.duration}
+ ${x.url}
+`
+        await conn.sendButt(m.chat, caption, wm, x.thumbnail, [['gore', '/gore']], m)
+}
+handler.help = ['gore']
+handler.tags = ['random']
+handler.command = /^(gore)$/i
+handler.limit = true
+handler.premium = true
+module.exports = handler
