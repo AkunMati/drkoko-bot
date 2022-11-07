5727da927362b3cddd82221ca4b2aa36703b8b5e
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-08 01:46:03 +0700

    Update random-meme.js

--- plugins/random-meme.js
+++ plugins/random-meme.js
@@ -1,12 +1,10 @@
-let axios = require("axios")
-let handler = async (m, {command, conn}) => {
-let res = await axios(`https://meme-api.herokuapp.com/gimme`)
-let json = res.data
-let url = json.url
-conn.sendButt(m.chat, `*${command}*`.trim(), wm, url, [['🔄 NEXT 🔄'], [`.meme`]], m)
+let handler = async (m, { conn, command }) => {
+let tioapikey = `https://xteam.xyz/randomimage/meme2?APIKEY=8872c3ff7452002c`
+    conn.sendButtonImg(m.chat, tioapikey, '*Nih Kak>_<*\n\n_*Jangan Lupa Subscribe YT OwnerKu Kak:*_\nhttps://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ', wm, 'Next', `.${command}`, m) 
 }
 handler.help = ['meme']
 handler.tags = ['random']
 handler.command = /^(meme)$/i
 handler.limit = true
+
 module.exports = handler
