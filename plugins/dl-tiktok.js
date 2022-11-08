a5545c58f7bddd4a5ebffc509b0133b1d9c8648d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-08 19:31:56 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,13 +1,39 @@
-const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
+// --------------------------------------------- pakai scrape social_media_downloader --------------------------------------------------------
 
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, '⫹⫺ Udah di kasih bilang apa?', m)
+let fetch = require('node-fetch')
+let axios = require('axios')
+const { tiktok } = require('social_media_downloader')
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+if (!args[0]) throw 'Masukkan Link'
+m.react('⏱️')
+try {
+let p = await tiktok(args[0])
+    if (!p.link) throw 'Can\'t download video!'
+    let cap = `*「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
+                 ████████▀▀▀████
+                 ████████────▀██
+                 ████████──█▄──█
+                 ███▀▀▀██──█████
+                 █▀──▄▄██──█████
+                 █──█████──█████
+                 █▄──▀▀▀──▄█████
+                 ███▄▄▄▄▄███████
+────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────
+*Nickname:* ${p.dev}
+*Description:* ${p.description}
+*Url:* ${p.url}
+
+_©ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×_
+`.trim()
+conn.send2ButtonVid(m.chat, p.link, cap, wm, `💰SEWA`, `.sewa`, `AUDIO✅`, `.tomp3`, fake, { quoted: adReply})
+} catch (e) {
+    throw eror
+    }
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
-handler.premium = true
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 handler.limit = true
+handler.premium = false
 module.exports = handler
