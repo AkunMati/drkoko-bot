c17e66b1618a6a0697417efb2ee3dabfc0e3e101
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-08 09:32:50 +0700

    Update random-wallpaperanim.js

--- plugins/random-wallpaperanim.js
+++ plugins/random-wallpaperanim.js
@@ -1,9 +1,18 @@
-let handler = async (m, { conn, command }) => {
-let nyenye = `http://hadi-api.herokuapp.com/api/walpaperanime`
-    conn.sendButtonImg(m.chat, nyenye, '*Nih Kak^_^*\n\n_*Jangan Lupa Subscribe YT OwnerKu Kak:*_\nhttps://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ', wm, 'Next', `.wallpaperanime`, m) 
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn }) => {
+    try {
+        let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
+        if (res.status != 200) throw await res.text()
+        let img = await res.buffer()
+        conn.sendFile(m.chat, img, '', '*Wibu Stress*', m, false, { thumbnail: Buffer.alloc(0) })
+    } catch (e) {
+        throw `Limit apikey habis atau error!`
+    }
 }
 handler.help = ['wallpaperanime']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(wallpaper|wp)anime$/i
 handler.limit = true
+
 module.exports = handler
