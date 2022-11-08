bd0f7a0bafe8b39b884bb6453bffa71af69b717a
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-08 08:54:13 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,26 +1,9 @@
-let { tiktokdl } = require('@bochilteam/scraper')
-let { tiktok } = require('../lib/scrape')
-let fetch = require('node-fetch')
+const hxz = require("hxz-api")
+let handler = async(m,{text, conn}) => {
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await conn.getName(who)
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-try {
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-let caption = `*Nickname:* ${nickname}
-*Description:* ${description}`
-  conn.sendButtonVid(m.chat, url, caption, author, 'To mp3', '.tomp3', fakes, adReply)
-} catch {
-const { res } = await aiovideodl(args[0])
-    const urll = res.data.url
-    if (!urll) throw 'Can\'t download video!'
-let caption = `*Nickname:* ${wm}`
-  conn.sendButtonVid(m.chat, urll, caption, author, 'To mp3', '.tomp3', fakes, adReply)
-}
+let p = await  hxz.ttdownloader(text)
+const { nowm, wm, audio } = p
+conn.sendFile(m.chat, nowm, null, '⫹⫺ Udah di kasih bilang apa?', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
