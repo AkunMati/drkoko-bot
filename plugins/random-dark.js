70af9c9aac8769eb0da7dd88699de99937ccee49
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-09 23:07:37 +0700

    Update random-dark.js

--- plugins/random-dark.js
+++ plugins/random-dark.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://api.xteam.xyz/randomimage/${command}?APIKEY=8872c3ff7452002c`
-    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
+    conn.sendButtonImg(m.chat, nyenye, '*Nih Kak>_<*\n\n_*Jangan Lupa Subscribe YT OwnerKu Kak:*_\nhttps://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ', wm, 'Next', `.${command}`, m) 
 }
 handler.help = ['dark']
-handler.tags = ['fun']
+handler.tags = ['random']
 handler.command = /^(dark)$/i
 handler.limit = true
 module.exports = handler
