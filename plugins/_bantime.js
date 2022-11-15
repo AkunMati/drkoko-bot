05afe8bb3ac870936bbf0823b2d0928fde4cf8cf
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-15 08:32:26 +0700

    Update _bantime.js

--- plugins/_bantime.js
+++ plugins/_bantime.js
@@ -1,6 +1,6 @@
-import db from '../lib/database.js'
-
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
+let db = require('../lib/database')
+let handler = m => m
+handler.before = async function(m, { conn, isAdmin, isBotAdmin }) {
 	if (m.isBaileys && m.fromMe) return !0
 	let user = db.data.users[m.sender]
 	if (user.permaban) return !0
