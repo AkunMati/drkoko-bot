2efc16b564fd701488adbc487bf5f9b0fb1f5066
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-15 08:56:23 +0700

    Update random-meme.js

--- plugins/random-meme.js
+++ plugins/random-meme.js
@@ -1,10 +1,39 @@
-let handler = async (m, { conn, command }) => {
-let tioapikey = `https://xteam.xyz/randomimage/meme2?APIKEY=8872c3ff7452002c`
-    conn.sendButtonImg(m.chat, tioapikey, '*Nih Kak>_<*\n\n_*Jangan Lupa Subscribe YT OwnerKu Kak:*_\nhttps://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ', wm, 'Next', `.${command}`, m) 
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+	try {
+		let anu = await fetch(`https://candaan-api.vercel.app/api/image`)
+			let json = await anu.json()
+			let fimg = await fetch(json.data[Math.floor(Math.random() * json.data.length)].url)
+			let fimgb = Buffer.from(await fimg.arrayBuffer())
+			if (Buffer.byteLength(fimgb) < 22000) throw new e()
+			await conn.sendButtonImg(m.chat, `_© meme cringe_`, packname + ' - ' + author, fimgb, `⧑ next ${command} ⧑`, `${usedPrefix + command}`, m)
+	} catch (e) {
+		try {
+			let anu = await fetch(`https://candaan-api.vercel.app/api/image/random`)
+			let json = await anu.json()
+			let fimg = await fetch(json.data.url)
+			let fimgb = Buffer.from(await fimg.arrayBuffer())
+			if (Buffer.byteLength(fimgb) < 22000) throw new e()
+			await conn.sendButton(m.chat, `_© meme cringe_`, packname + ' - ' + author, fimgb, [[`⧑ next ${command} ⧑`, `${usedPrefix + command}`]], m)
+		} catch (e) {
+			try {
+				let fimg = await fetch(`https://api.lolhuman.xyz/api/meme/memeindo?apikey=${global.api}`)
+				let fimgb = Buffer.from(await fimg.arrayBuffer())
+				if (Buffer.byteLength(fimgb) < 22000) throw new e()
+				await conn.sendButtonImg(m.chat, `_© meme cringe_`, packname + ' - ' + author, fimgb, `⧑ next ${command} ⧑`, `${usedPrefix + command}`, m)
+			} catch (e) {
+				m.reply(`Terjadi kesalahan, coba lagi nanti.`)
+			}
+		}
+	}
 }
+
 handler.help = ['meme']
 handler.tags = ['random']
-handler.command = /^(meme)$/i
+handler.command = /^((random)?meme)$/i
+
+handler.premium = false
 handler.limit = true
 
 module.exports = handler
