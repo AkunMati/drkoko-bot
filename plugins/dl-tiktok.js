7f0156637d0c1a38ea8c4b734e1c084c4cdb90f4
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-16 01:21:13 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,15 +1,21 @@
-let https = require('axios')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
-let tio = (await https.get(API('males', '/tiktok', { url: args[0] } ))).data;
-if (tio.status != 200) throw tio.message;
-if (!tio) throw tio.message;
- let hasilnya = `*Title:* ${tio.title}\n\n*Author:* ${tio.author}`
-  conn.sendButtonVid(m.chat, tio.video, hasilnya, wm, `Back`, `.menu`, m)
-        }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+let axios = require('axios')
+let handler = async (m, {conn, args, usedPrefix, command }) => {
+	if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
+	if (!args[0].match(/tiktok/gi)) throw `url salah`
+	try {
+		let a = await axios.get('https://rest-beni.herokuapp.com/api/tiktok?url=' + args[0])
+		if (!a.data.result.video_original) {
+			conn.sendFile(m.chat, a.data.result.video, '', '\n\nKOKO PANGERAN', m)
+		} else {
+			conn.sendFile(m.chat, a.data.result.video_original, "", "\n\nKOKO PANGERAN", m)
+		}
+	} catch (e) {
+log(e)
+		conn.reply(m.chat, "error memek", m)
+	}
+}
+handler.help = ['tiktok <url>']
 handler.tags = ['downloader']
-handler.command = /^(tiktok|ttdl|tt|tiktokdl|tiktoknowm)$/i
+handler.command = /^(tiktok)$/i
 handler.limit = true
-handler.premium = true
 module.exports = handler
