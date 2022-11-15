fec97ded61d865fcbcfd56b41287c0fa071a4191
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-15 08:19:05 +0700

    Create _mute.js

---
+++ plugins/_mute.js
@@ -0,0 +1,17 @@
+import db from '../lib/database.js'
+
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+	if (m.isBaileys && m.fromMe) return !0
+	if (m.isGroup) {
+		let chat = db.data.chats[m.chat]
+		if (chat.permaBan) return !0
+		if (chat.mutecd == 0) return !0
+		if (new Date - chat.lastmute <= chat.mutecd) return !0
+		chat.lastmute = 0
+		chat.mutecd = 0
+		chat.isBanned = false
+		await this.sendMessage(m.chat, { text: `Bot dapat digunakan kembali.` }, { quoted: fkontak })
+	}
+	return !0
+}
+module.exports = handler
