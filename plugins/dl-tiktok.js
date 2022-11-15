1d99b454a0e843f2798f2680478215d6f8a1df96
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-16 01:31:23 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,13 +1,14 @@
 let axios = require('axios')
 let handler = async (m, {conn, args, usedPrefix, command }) => {
 	if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
+        m.react('⏱️')
 	if (!args[0].match(/tiktok/gi)) throw `url salah`
 	try {
 		let a = await axios.get('https://rest-beni.herokuapp.com/api/tiktok?url=' + args[0])
 		if (!a.data.result.video_original) {
-			conn.sendFile(m.chat, a.data.result.video, '', '\n\nKOKO PANGERAN', m)
+			conn.sendButtonVid(m.chat, a.data.result.video, '', '\n\nKOKO PANGERAN', wm, `Audio`, `.tomp3`, m)
 		} else {
-			conn.sendFile(m.chat, a.data.result.video_original, "", "\n\nKOKO PANGERAN", m)
+			conn.sendButtonVid(m.chat, a.data.result.video_original, '', '\n\nKOKO PANGERAN', wm, `Audio`, `.tomp3`, m)
 		}
 	} catch (e) {
 log(e)
@@ -17,5 +18,6 @@ log(e)
 handler.help = ['tiktok <url>']
 handler.tags = ['downloader']
 handler.command = /^(tiktok)$/i
+handler.premium = true
 handler.limit = true
 module.exports = handler
