f39e7b1d97ecb9840cdbfc337b881e9c13b02667
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-15 11:22:15 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -77,6 +77,9 @@ module.exports = {
                     if (!isNumber(user.anjinglastclaim)) user.anjinglastclaim = 0
                     if (!isNumber(user.area)) user.area = 0
                     if (!('banned' in user)) user.banned = false
+                    if (!isNumber(user.bannedcd)) user.bannedcd = 0
+                    if (!('permaban' in user)) user.permaban = false
+                    if (!isNumber(user.lastbanned)) user.lastbanned = 0
                     if (!isNumber(user.warn)) user.warn = 0
                     if (!isNumber(user.warning)) user.warning = 0
                     if (!isNumber(user.lastIstigfar)) user.lastIstigfar = 0
@@ -187,6 +190,9 @@ module.exports = {
                     anjinglastclaim: 0,
                     area: 0,
                     banned: false,
+                    bannedcd: 0,
+                    permaban: false,
+                    lastbanned: 0,
                     warn: 0,
                     warning: 0,
                     lastIstigfar: 0,
@@ -264,6 +270,7 @@ module.exports = {
                     if (!('closeGroup' in chat)) chat.closeGroup = false
                     if (!isNumber(chat.add)) chat.add = 0
                     if (!('isBanned' in chat)) chat.isBanned = false
+                    if (!('permaBan' in chat)) chat.permaBan = false
                     if (!('welcome' in chat)) chat.welcome = true
                     if (!('detect' in chat)) chat.detect = true
                     if (!('sWelcome' in chat)) chat.sWelcome = ''
@@ -301,8 +308,9 @@ module.exports = {
                     closeGroup: false,
                     add: 0,
                     isBanned: false,
+                    permaBan: false,
                     welcome: true,
-                    detect: true,
+                    detect: false,
                     sWelcome: '',
                     sBye: '',
                     sPromote: '',
@@ -318,7 +326,7 @@ module.exports = {
                     expired: 0,
                     antiBadword: true,
                     antiDownload: false,
-                    antispam: false,
+                    antispam: true,
                     antitroli: true,
                     antivirtex: true,
                     antiVirus: true,
