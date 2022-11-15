85db5cb9e5c207fd63cb10426e83a715eedc741e
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-15 08:25:29 +0700

    Update dl-ytv720.js

--- plugins/dl-ytv720.js
+++ plugins/dl-ytv720.js
@@ -1,19 +1,60 @@
-const xfar = require('xfarr-api')
-const fetch = require('node-fetch')
-let handler = async (m, { conn, command, text }) => {
-    if (!text) throw 'Masukkan Link\n\nContoh: .ytv720 https://youtube.com/xxxxxx'
-  let res = await xfar.Youtube(text)
-m.react('⏱️')
-m.reply('*Tunggu Sebentar...*')
-conn.sendFile(m.chat,res.medias[2].url, '', `Youtube Downloader
-720p
-jika masih blur, berarti videonya emang ngeblur dari youtubenya
-Atau bisa jadi faktor *MUKA*`, m)
+let fetch = require('node-fetch')
+const { savefrom, youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
 
+const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   
+function niceBytes(x) {
+	let l = 0, n = parseInt(x, 10) || 0;
+	while(n >= 1024 && ++l){
+		n = n/1024;
+	}
+	return(n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
 }
-handler.help = ['ytv720 <url>']
-handler.tags = ['internet']
-handler.command = /^ytv720$/i
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+	if (!text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))) return m.reply(`Invalid Youtube URL.`)
+	let fimg, fimgb
+	let za = command.includes('480') ? '480' : command.includes('720') ? '720' : '1080'
+	try {
+		const { thumbnail, video: _video, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+		let video, source, res, link, lastError
+		video = _video[`${za}p`]
+		if (video.fileSize > 200000) return m.reply(`Filesize: ${video.fileSizeH}\nTidak dapat mengirim, maksimal file 200 MB`)
+		link = await video.download()
+		if (link) res = await fetch(link)
+		if (res) source = await res.arrayBuffer()
+		let ini_txt = `*${title}*\n\n`
+		ini_txt += `⭔ Watch : ${text}\n`
+		ini_txt += `⭔ Resolution : ${video.quality}\n`
+		ini_txt += `⭔ Size : ${video.fileSizeH}`
+		fimg = await fetch(link)
+		fimgb = Buffer.from(await fimg.arrayBuffer())
+		if (Buffer.byteLength(fimgb) < 22000) throw new e()
+		await conn.sendMessage(m.chat, { video: fimgb, caption: ini_txt }, { quoted: m })
+	} catch (e) {
+		try {
+			let anu = await savefrom(text)
+			var x = anu.url.findIndex(y => y.quality ===za && y.ext==="mp4")
+			if (anu.url[x].filesize > 209715200) return m.reply(`Filesize: ${niceBytes(anu.url[x].filesize)}\nTidak dapat mengirim, maksimal file 200 MB`)
+			fimg = await fetch(anu.url[x].url)
+			fimgb = Buffer.from(await fimg.arrayBuffer())
+			if (Buffer.byteLength(fimgb) < 22000) throw new e()
+			let ini_txt = `*${anu.meta.title}*\n\n`
+			ini_txt += `⭔ Watch : ${anu.meta.source}\n`
+			ini_txt += `⭔ Resolution : ${anu.url[x].quality}p\n`
+			ini_txt += `⭔ Size : ${niceBytes(anu.url[x].filesize)}`
+			await conn.sendMessage(m.chat, { video: fimgb, caption: ini_txt }, { quoted: m })
+		} catch (e) {
+			m.reply(`[!] ${za}p tidak tersedia / terjadi kesalahan.`)
+		}
+	}
+}
+
+handler.menudownload = ['ytvideo480','ytvideo720','ytvideo1080'].map(v => v + ' <url>')
+handler.tagsdownload = ['downloader']
+handler.command = /^(yt(v(ideo)?|mp4)(480|720|1080)p?)$/i
+
 handler.premium = true
 handler.limit = true
+
 module.exports = handler
