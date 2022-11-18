9487dd2c59bf041724f2fd480d5136a2b868dad1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-19 05:04:02 +0700

    Create add-iklan.js

---
+++ plugins/add-iklan.js
@@ -0,0 +1,18 @@
+let fs = require('fs')
+let handler = async (m, {conn, usedPrefix, text, command}) => {
+  const json = JSON.parse(fs.readFileSync('./db/store.json'))
+    let [t, n, d] = text.split`#`
+    if (!t) throw `「 ❗ 」\nContoh pengunaan : \n${usedPrefix + command} <judul>#<nomor>#<desk>`
+    if (!n) throw `「 ❗ 」\nContoh pengunaan : \n${usedPrefix + command} <judul>#<nomor>#<desk>`
+    if (!d) throw `「 ❗ 」\nContoh pengunaan : \n${usedPrefix + command} <judul>#<nomor>#<desk>`
+    
+    json.push({name: t, desc: d, no: n})
+    fs.writeFileSync('./db/store.json', JSON.stringify(json))
+    m.reply('「 ✔️ 」\nBerhasil Menambahkan Iklan')
+}
+handler.tags = ['owner']
+handler.help = ['addiklan']
+handler.command = /^(addiklan)$/i
+handler.owner = true
+
+module.exports = handler
