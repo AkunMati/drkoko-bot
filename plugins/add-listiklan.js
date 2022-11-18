7eff63639deeeebf4967bf81428e924a721dd0d3
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-19 05:26:55 +0700

    Create add-listiklan.js

---
+++ plugins/add-listiklan.js
@@ -0,0 +1,25 @@
+let fs = require('fs')
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let json = JSON.parse(fs.readFileSync(`./src/iklan.json`))
+    let row =  []
+for (let i of json) {
+	      row.push({
+	title: `${i.name}\n`,
+		description: `🎟Nama Iklan : ${i.name}\n:👤Pemilik Iklan : wa.me/${i.no}\n🖼Deskripsi : ${i.desc}`,
+		rowId: ' '
+       }
+    )
+    }
+	let button = {
+		buttonText: 'LIST IKLAN',
+		description: 'Berikut daftar iklan yang terdaftar di database',
+		footerText: wm
+	}
+	return conn.sendListM(m.chat, button, row, m)
+
+}
+handler.help = ['iklan'].map(v => 'list' + v)
+handler.tags = ['iklan']
+handler.command = /^(listiklan|iklan)$/i
+
+module.exports = handler
