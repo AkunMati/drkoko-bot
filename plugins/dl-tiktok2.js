bd3680a9c84ea388624913f42a2ec66df22ffdba
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-19 04:45:48 +0700

    Update dl-tiktok2.js

--- plugins/dl-tiktok2.js
+++ plugins/dl-tiktok2.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-pangeran = `5d1197db351b`
+pangeran = `5d1197db351b`;
   if (!text) return conn.reply(m.chat, `Gunakan format: ${usedPrefix}${command} Colin Changed`, m)
 
   await conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/700df524f39be728c8882.jpg', 'Searching...', wm, '😳KOKO', '.owner', m)
