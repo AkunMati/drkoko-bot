ec61e2b7f355359aeb6889366bec7c6b8509e280
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-22 05:12:54 +0700

    Update bot-samasama.js

--- plugins/bot-samasama.js
+++ plugins/bot-samasama.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn }) => {
   let name = await conn.getName(m.sender)
-  let res = await `https://api.memegen.link/images/custom/sama_sama_kak/${name}.jpg?background=https%3A%2F%2Ftelegra.ph%2Ffile%2F06a27a93b66f63a5a067e.png&watermark=memecomplete.com&token=khj7yfpkw8idaztccuys`
+  let res = await `https://api.memegen.link/images/custom/sama_sama_kak/${name}.jpg?background=https%3A%2F%2Ftelegra.ph%2Ffile%2F77c163345d01414345ab9.png&watermark=memecomplete.com&token=khj7yfpkw8idaztccuys`
   let stiker = await sticker(null, res, global.packname, global.author)
   conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
 }
