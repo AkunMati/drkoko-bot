fcf1c902d322427ce3e919a3e8198363ce29ac05
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-22 04:30:28 +0700

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,15 +1,15 @@
-const instagramGetUrl = require('instagram-url-direct')
-let handler = async (m, { conn, args, command, usedPrefix }) => {
+let instagramGetUrl = require('instagram-url-direct')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    let res = await instagramGetUrl(args[0])
-    for (let result of res.url_list) {
-    conn.sendFile(m.chat, result, 'instagram.mp4', author, m)
-  }
+    const results = (await instagramGetUrl(args[0])).url_list[0]
+
+    conn.sendFile(m.chat, results, 'ig.mp4', `*Nih Kak>_<*\n_Lain Kali Download Sendiri Kak😖_\n\n_*Jangan Lupa Subscribe My Channel:*_\nhttps://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ`, m)
 }
-handler.help = ['instagram <url>']
+handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^i(nsta)?g(ram)?(dl)?$/i
-handler.premium = true
+
+handler.command = /^(instagram|ig|igdl)$/i
 handler.limit = true
+handler.premium = true
 
 module.exports = handler
