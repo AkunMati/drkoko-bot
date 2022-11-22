cc7f94cb0129c5105fe768bb1912ae22c32b6f3f
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-23 01:25:00 +0700

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,36 +1,17 @@
-let fetch = require('node-fetch')
-let axios = require('axios')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw 'Uhm... url nya mana ?'
-let res = await fetch(API('xteam', '/dl/mediafire', { url: args[0] }, 'apikey'))
-    let json = await res.json()
-    
-    m.reply(data.wait)
-await conn.reply(m.chat, `Downloading media from MediaFire`, 0, {
-  contextInfo: { mentionedJid: [m.sender],
-    externalAdReply :{
-    mediaUrl: data.sc,
-    mediaType: 2,
-    description: data.deslink , 
-    title: run,
-    body: wm,
-    thumbnail: await(await fetch(img)).buffer(),
-    sourceUrl: data.sc
-     }}
-  }) 
-let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}
-📂 *Size*: ${json.result.size}
+const { lookup } = require ('mime-types')
+const { mediafiredl } = require ('@bochilteam/scraper')
 
-`.trim()
-m.reply(txt)
-   // await conn.sendFile(m.chat, json.result.url, `${json.result.title}`, m , null )
-await conn.sendMessage(m.chat, { document: { url: json.result.url}, mimetype: 'document', fileName: `${json.result.title}`}, {quoted: m})
-
+let handler = async (m, { conn, args }) => {
+	if (!args[0]) throw 'Input URL' 
+	if (!/https?:\/\/(www\.)?mediafire\.com/.test(args[0])) throw 'Invalid URL' 
+	let res = await mediafiredl(args[0])
+	let mimetype = await lookup(res.url)
+	delete res.url2
+	m.reply(Object.keys(res).map(v => `*• ${v.capitalize()}:* ${res[v]}`).join('\n') + '\n\n_Sending file..._')
+	conn.sendMessage(m.chat, { document: { url: res.url }, fileName: res.filename, mimetype }, { quoted: m })
 }
-handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.help = handler.alias = ['mediafire']
 handler.tags = ['downloader']
-handler.command = /^(mediafire|mf)?$/i
-
-
+handler.command = /^(mediafire)$/i
 
 module.exports = handler
