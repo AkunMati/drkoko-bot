37e459ca27a4e8a6bb16361e7ab8dc9dada857c2
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-24 02:35:40 +0700

    Create nsfw-hentaivt.js

---
+++ plugins/nsfw-hentaivt.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, command }) => {
+
+	let ne = await (await fetch('https://raw.githubusercontent.com/NdaaXD/database/main/nsfw/hentaivid.json')).json()
+
+    
+
+    let neko = conn.pickRandom(ne)
+
+	conn.sendFile(m.chat, neko, 'hentai.mp4', 'By NdaaBotz-MD',m)
+
+}
+
+handler.command = /^(hentaivt)$/i
+
+handler.premium = true
+
+//By Koko Pangeran
+
+module.exports = handler
