b53b0dcf4b6a1ac3d820be68675eb2fe100b2200
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-24 05:07:02 +0700

    Update sticker-scircle.js

--- plugins/sticker-scircle.js
+++ plugins/sticker-scircle.js
@@ -1,6 +1,7 @@
 let uploadImage = require('../lib/uploadImage')
 let { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text }) => {
+m.reply('Tunggu Sebentar...!')
 try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
@@ -10,7 +11,7 @@ let scircle = global.API('dzx', '/api/canvas/circle', { url })
 let stiker = await sticker(null, scircle, global.packname, global.author)
 conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, { asSticker: true })
 } catch (e) {
-m.reply(`*[SUCCES❗]*\n_*Berhasil Picture Menjadi Sticker Bulet!*_`)
+m.reply(`*[ERROR❌]*\n_*Terjadi Kesalahan, Reply Picture Ketik .scircle!*_`)
 }}
 handler.help = ['scircle']
 handler.tags = ['sticker']
