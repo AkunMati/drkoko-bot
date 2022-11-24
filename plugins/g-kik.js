eb17b3b9905e463fc482b2eb028bc1010bd47c52
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-24 07:49:53 +0700

    Update g-kik.js

--- plugins/g-kik.js
+++ plugins/g-kik.js
@@ -3,7 +3,7 @@ let fs = require('fs')
  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
   aki = m.quoted ? [m.quoted.sender] : m.mentionedJid
   let users = aki.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
-  wayy = '_*Gw punya Fitur Kick LHO 😐'
+  wayy = '_*Gw Punya Fitur Kick LHO 😐*_'
   for (let i of users) {
   wayy += ` @${i.split('@')[0]}`
   }
