e06665253bee2db0227c7ba337d967b63d11d912
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-12-03 03:17:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Readline = require('readline')
 const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 
-CFonts.say('Leviathan', {
+CFonts.say('drkoko-bot', {
   colors: ['#f2aa4c'],
   font: 'block',
   align: 'center',
