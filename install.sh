e6bd3afd1041879a101856d0bcdd0994ca5a5f07
AkunMati <119078661+AkunMati@users.noreply.github.com>
2022-12-03 02:18:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ pkg install git -y
 pkg install nodejs -y
 pkg install ffmpeg -y
 pkg install imagemagick -y
-pkg install yarn
+pkg install npm
 git clone https://github.com/MendingTuru/Bottom-MD2
 cd Bottom-MD2
-yarn
+npm install
 node .
