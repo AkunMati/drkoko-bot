a5d5843c8c63df6a1aaac35391666dda017c5777
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-12-03 03:16:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-console.log("Running...");
-let cluster = require("cluster");
-let path = require("path");
-let fs = require("fs");
-let package = require("./package.json");
-const CFonts = require("cfonts");
-const Readline = require("readline");
-const yargs = require("yargs/yargs");
+console.log('Running...')
+let cluster = require('cluster')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts = require('cfonts')
+const Readline = require('readline')
+const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 
-CFonts.say('Bottom-MD2', {
+CFonts.say('Leviathan', {
   colors: ['#f2aa4c'],
   font: 'block',
   align: 'center',
@@ -19,7 +19,7 @@ CFonts.say(`${package.name} Di Buat Oleh @${package.author.name || package.autho
   align: 'center',
 })
 
-var isRunning = false;
+var isRunning = false
 /**
  * Start a js file
  * @param {String} file `path/to/file`
@@ -27,44 +27,44 @@ var isRunning = false;
 function start(file) {
   if (isRunning) return
   isRunning = true
-  let args = [path.join(__dirname, file), ...process.argv.slice(2)];
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
   CFonts.say([process.argv[0], ...args].join(' '), {
     font: 'console',
     align: 'center',
     gradient: ['red', 'magenta']
-  });
+  })
   cluster.setupMaster({
     exec: path.join(__dirname, file),
     args: args.slice(1),
-  });
+  })
   let p = cluster.fork()
-  p.on("message", data => {
-    console.log("[RECEIVED]", data);
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
     switch (data) {
-      case "reset":
-        p.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
-      case "uptime":
-        p.send(process.uptime());
-        break;
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
     }
-  });
-  p.on("exit", code => {
-    isRunning = false;
-    console.error("Exited with code:", code);
-    if (code === 0) return;
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Exited with code:', code)
+    if (code === 0) return
     fs.watchFile(args[0], () => {
-      fs.unwatchFile(args[0]);
+      fs.unwatchFile(args[0])
       start("main.js");
-    });
-  });
+    })
+  })
   let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
   if (!opts['test'])
     if (!rl.listenerCount()) rl.on('line', line => {
       p.emit('message', line.trim())
-    });
+    })
   // console.log(p)
 }
 
