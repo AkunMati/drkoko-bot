efec12926a542db856a86784b49d5b89d13ea724
AkunMati <119078661+AkunMati@users.noreply.github.com>
2022-12-04 14:36:20 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -654,26 +654,15 @@ module.exports = {
                 if (chat.welcome) {
                     let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
                     for (let user of participants) {
-                       let pp = 'https://telegra.ph/file/e641e98d956904c503e86.jpg'
+                       let pp = 'https://i.ibb.co/R9Hx76q/drkoko-bot.jpg'
                         try {
                             pp = await this.profilePictureUrl(user, 'image')
                         } catch (e) {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup @user').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang @user!')).replace('@user', '@' + user.split('@')[0]).replace('@subject', groupMetadata.subject)
-                                this.send2ButtonImg(id, pp, text, wm, "⎙ Menu", ".menu", "⎙ Intro", ".intro", null, fake,{
-  contextInfo: { externalAdReply :{
-    showAdAttribution: true,
-    mediaUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ',
-    mediaType: 2,
-    description: data.deslink, 
-    title: run,
-    body: wm,
-    thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: 'https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ'
-     }}
-  })
-                        }
+                                this.send2ButtonLoc(id, pp, text, wm3, "⎙ Menu", ".menu", "⎙ Intro", "intro", null)
+                                }
                     }
                 }
                 break
