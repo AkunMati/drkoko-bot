6457a44ea978edac9da15ae175d484bb1946fca9
AkunMati <119078661+AkunMati@users.noreply.github.com>
2022-12-04 17:28:14 +0700

    Update g-tagall.js

--- plugins/g-tagall.js
+++ plugins/g-tagall.js
@@ -3,14 +3,10 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
                 global.dfail('admin', m, conn)
                 throw false
                 }
-  let replace = { 
-     readmore: readMore 
-  }
-  let teks = `┌─「 TAG ALL 」\n\n@${m.sender.split('@')[0]} : ${text ? text : ''} %readmore\n\n`
+  let teks = `┌─「 TAG ALL 」\n\n@${m.sender.split('@')[0]} : ${text ? text : ''}\n\n`
   for (let mem of participants) {
   teks += `├✪➢ @${mem.id.split('@')[0]}\n`}
   teks += `└────\n`
-  text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <message>']
